1f263d58ccf972eddacc3050068f58015d030367
jonghyouk yun <ageldama@gmail.com>
2014-03-22 10:39:31 +0900

    merged.

--- lisp/start-swank.lisp
+++
@@ -1,6 +0,0 @@
-;;(load #p"/home/adaltan/lisp/asdf/asdf.lisp")
-;;(push #P"/home/adaltan/lisp/slime-2010-11-05/" asdf:*central-registry*)
-
-(asdf:load-system :swank)
-(swank:create-server :port 4005
-  :dont-close t :coding-system "utf-8-unix")
