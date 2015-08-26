057ead56d169a653244cb57f89c3abe5c721b123
jonghyouk yun <ageldama@gmail.com>
2015-08-26 17:21:09 +0900

    .caps.

--- on-vga-0-xrandr.sh
+++ on-vga-0-xrandr.sh
@@ -1,4 +1,8 @@
  xrandr --output VGA-0 --mode 1280x1024 --left-of LVDS
  xrandr --output LVDS --mode 1366x768
 
+xmodmap -e 'keycode 66 = Control_L'
+xmodmap -e 'clear Lock'
+xmodmap -e 'add Control = Control_L'
+
 
