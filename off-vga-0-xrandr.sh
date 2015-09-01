64706bb3c1dc51020218c6d98c6f320b2d83376f
jonghyouk yun <ageldama@gmail.com>
2015-09-01 19:03:46 +0900

    ...

--- off-vga-0-xrandr.sh
+++ off-vga-0-xrandr.sh
@@ -1,5 +1,6 @@
-xrandr --output VGA-0 --off
-xrandr --output LVDS --mode 1366x768
+xrandr --output VGA-0 --off \
+    --output HDMI-0 --off \
+    --output LVDS --mode 1366x768
 
 xmodmap -e 'keycode 66 = Control_L'
 xmodmap -e 'clear Lock'
