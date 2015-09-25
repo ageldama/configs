ec3549de1d98a5bad133e7bd82b4ee57a8762a08
jonghyouk yun <ageldama@gmail.com>
2015-09-26 08:18:21 +0900

    emacs color-theme-solarized.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -162,14 +162,13 @@
 
 
 
-;; (when window-system
-;;     (load-theme 'light-blue))
-;; (when window-system
-;;   (load-theme 'tronesque))
+(when (and window-system
+           (string-equal system-type "gnu/linux"))
+  ;;(load-theme 'alect-light)
+  (load-theme 'solarized)
+  ;;(load-theme 'leuven)
+  )
 
-;; (when (and window-system
-;;            (not (string-equal system-type "darwin")))
-;;   (load-theme 'misterioso))
 
 (unless window-system
   (progn
@@ -197,18 +196,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "869b11b64da20b6b04e9b18721e03a58e5d9f0ee3a7a91bfe7cdc2b24a828109" "e890fd7b5137356ef5b88be1350acf94af90d9d6dd5c234978cd59a6b873ea94" default))))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "869b11b64da20b6b04e9b18721e03a58e5d9f0ee3a7a91bfe7cdc2b24a828109" "e890fd7b5137356ef5b88be1350acf94af90d9d6dd5c234978cd59a6b873ea94" default)))
+ '(line-number-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t (:background "white")))))
 
 
-;(load-theme 'deeper-blue)
-
-;(load "~/local/io-mode/io-mode.el")
 
 
 
@@ -289,6 +286,7 @@
 ;; (package-install 'helm)
 ;; (package-install 'unfill)
 ;; (package-install 'centered-window-mode)
+;; (package-install 'color-theme-solarized)
 
 (require 'helm-config)
 (helm-mode 1)
