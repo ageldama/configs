57011a1d848d953fabc9024106a005e1b2f4b5f6
jonghyouk yun <ageldama@gmail.com>
2015-11-28 15:33:16 +0900

    ...

---
+++ jdk-x-font-antialias.sh
@@ -0,0 +1,4 @@
+
+# /etc/profile.d/jdk-x-font-antialias.sh
+# https://wiki.archlinux.org/index.php/Java_Runtime_Environment_fonts#Basic_settings
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true'
