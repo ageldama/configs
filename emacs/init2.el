cefb591c235417c232970461b270f9b5301a5091
Jong-hyouk Yun <jong-hyouk.yun@zalando.de>
2016-11-19 09:32:36 +0100

    emacs powerline + golang.

--- emacs/init2.el
+++ emacs/init2.el
@@ -90,8 +90,8 @@
 
 
 ;;; user?
-(setq-default user-full-name "John, Jonghyouk Yun")
-(setq-default user-mail-address "ageldama@gmail.com")
+(setq-default user-full-name "Jong-Hyouk Yun")
+(setq-default user-mail-address "jong-hyouk.yun@zalando.de")
 
 
 ;;; grep-find, rgrep, find-grep-dired
@@ -384,27 +384,6 @@
   (exec-path-from-shell-initialize))
 
 
-;; NOTE: cargo install {rustfmt/cargo-check}.
-(use-package rust-mode :ensure t :pin melpa)
-(use-package toml-mode :ensure t :pin melpa)
-;;(use-package flycheck-rust :ensure t :pin melpa)
-(use-package cargo :ensure t :pin melpa)
-
-(use-package racer :ensure t :pin melpa)
-(use-package company-racer :ensure t :pin melpa)
-
-
-;;(setq flycheck-rust-cargo-executable "/usr/local/bin/cargo")
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
-
-(defun disable-flycheck-mode ()
-  (flycheck-mode -1))
-
-(add-hook 'rust-mode-hook #'disable-flycheck-mode)
-
-(define-key rust-mode-map  [(tab)] 'company-complete)
-
-
 
 ;; (use-package ox-gfm :ensure t )
 ;; (eval-after-load "org"
@@ -437,20 +416,37 @@
 (helm-projectile-on)
 
 
+
+;;; powerline.
+(use-package powerline :ensure t :pin melpa)
+(use-package airline-themes :ensure t :pin melpa)
+(require 'powerline)
+(require 'airline-themes)
+;;(airline-themes-solarized-gui)
+(load-theme 'airline-light)
+
+
+
+;;; go-lang.
+(use-package go-mode :ensure t :pin melpa)
+
+(when (and (fboundp 'go-mode) (memq window-system '(mac ns)))
+  (use-package exec-path-from-shell :ensure t :pin melpa)
+  (exec-path-from-shell-copy-env "GOROOT")
+  (exec-path-from-shell-copy-env "GOPATH"))
+
+(use-package go-eldoc :ensure t :pin melpa)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+
+(use-package go-autocomplete :ensure t :pin melpa)
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+
+
 ;;; for magit, ...
 (setenv "EDITOR" "emacsclient")
-
-
-
-
-;;; slime.
-(use-package slime :ensure t :pin melpa)
-
-(setq inferior-lisp-program "/Users/jhyun/local/sbcl-1.2.11-x86-64-darwin/run-sbcl.sh")
-;;(setq inferior-lisp-program "/usr/local/bin/ecl")
-(setq slime-contribs '(slime-fancy))
-;; (setq common-lisp-hyperspec-root (expand-file-name "~/local/HyperSpec/"))
-
 
 
 
