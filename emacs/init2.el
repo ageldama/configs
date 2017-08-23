e724bb7a803ecf11895646665dbf0361005bc2e5
jonghyouk yun <ageldama@gmail.com>
2017-08-23 21:54:35 +0900

    --wip-- [skip ci]

--- emacs/init2.el
+++ emacs/init2.el
@@ -398,6 +398,12 @@
   (load-theme #'leuven t)
   )
 
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+
 
 
 
