79f35211bf9239a1bc764931100dfbab8b81cc53
jonghyouk yun <ageldama@gmail.com>
2018-03-18 20:48:12 +0100

    --wip-- [skip ci]

--- emacs/init2.el
+++ emacs/init2.el
@@ -370,6 +370,75 @@
               (require 'slime)
               (slime-setup '(slime-fancy)))))
 
+;;; irony
+(when nil
+  (use-package irony :ensure t :pin melpa
+    :config (progn
+              ;; If irony server was never installed, install it.
+              (unless (irony--find-server-executable)
+                (call-interactively #'irony-install-server))
+              (add-hook 'c++-mode-hook 'irony-mode)
+              (add-hook 'c-mode-hook 'irony-mode)
+              ;; Use compilation database first, clang_complete as fallback.
+              (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang
+                                                              irony-cdb-clang-complete))
+              (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
+  ;; I use irony with company to get code completion.
+  (use-package company-irony :ensure t :pin melpa
+    :config (progn
+              (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))))
+  ;; I use irony with flycheck to get real-time syntax checking.
+  (use-package flycheck-irony :ensure t :pin melpa
+    :config (progn
+              (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))
+  ;; Eldoc shows argument list of the function you are currently writing in the echo area.
+  (use-package irony-eldoc :ensure t :pin melpa
+    :config (progn (add-hook 'irony-mode-hook #'irony-eldoc))))
+
+;;; rdm
+;;; http://martinsosic.com/development/emacs/2017/12/09/emacs-cpp-ide.html
+(when t
+  (use-package rtags :ensure t :pin melpa
+    :config
+    (progn
+      (unless (rtags-executable-find "rc") (error "Binary rc is not installed!"))
+      (unless (rtags-executable-find "rdm") (error "Binary rdm is not installed!"))
+      ;;
+      (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
+      (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
+      (define-key c-mode-base-map (kbd "M-?") 'rtags-display-summary)
+      (rtags-enable-standard-keybindings)
+      ;;
+      (setq rtags-use-helm t)
+      ;; Shutdown rdm when leaving emacs.
+      (add-hook 'kill-emacs-hook 'rtags-quit-rdm)))
+  ;; TODO: Has no coloring! How can I get coloring?
+  (use-package helm-rtags :ensure t :pin melpa
+    :config
+    (progn
+      (setq rtags-display-result-backend 'helm)))
+  ;; Use rtags for auto-completion.
+  (use-package company-rtags :ensure t :pin melpa
+    :config
+    (progn
+      (setq rtags-autostart-diagnostics t)
+      (rtags-diagnostics)
+      (setq rtags-completions-enabled t)
+      (push 'company-rtags company-backends)))
+  ;; Live code checking.
+  (use-package flycheck-rtags :ensure t :pin melpa
+    :config
+    (progn
+      ;; ensure that we use only rtags checking
+      ;; https://github.com/Andersbakken/rtags#optional-1
+      (defun setup-flycheck-rtags ()
+        (flycheck-select-checker 'rtags)
+        (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+        (setq-local flycheck-check-syntax-automatically nil)
+        (rtags-set-periodic-reparse-timeout 2.0)  ;; Run flycheck 2 seconds after being idle.
+        )
+      (add-hook 'c-mode-hook #'setup-flycheck-rtags)
+      (add-hook 'c++-mode-hook #'setup-flycheck-rtags))))
 
 
 ;;; EOF.
