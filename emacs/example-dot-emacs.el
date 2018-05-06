7ff783670dffebead8bb9013b51a8293cd7d3ba5
jonghyouk yun <ageldama@gmail.com>
2018-05-06 16:17:49 +0900

    ...

--- emacs/example-dot-emacs.el
+++ emacs/example-dot-emacs.el
@@ -1,3 +1,19 @@
+;;;; Code:
 (load-file (expand-file-name "~/Projects/configs/emacs/dot-emacs-2018"))
-(load-file (expand-file-name "~/Projects/configs/emacs/cmake-ide/cmake-ide.el"))
 
+;;; simplest layer support.
+(defvar load-layer-base-path (expand-file-name "~/Projects/configs/emacs/"))
+
+(defun load-layer (layer-name)
+  "Specify LAYER-NAME as elisp filename to load."
+  (interactive "fFile to load:")
+  (load-file (format "%s%s" load-layer-base-path layer-name)))
+
+(defun load-cmake-ide-layer ()
+  "Just load cmake-ide layer."
+  (interactive)
+  (load-layer "cmake-ide/cmake-ide.el"))
+
+
+
+;;;EOF.
