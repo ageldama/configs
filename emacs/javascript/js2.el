2cb2b864457b7b5d5f8cdc07547025949fb78ab4
Jong-Hyouk Yun <jonghyouk.yun@streami.co>
2018-06-07 13:03:10 +0900

    --wip-- [skip ci]

---
+++ emacs/javascript/js2.el
@@ -0,0 +1,47 @@
+;;; js2-mode.
+(use-package js2-mode :ensure t :pin melpa
+  :config (progn (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
+		 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))))
+
+;;; json-mode.
+(use-package json-mode :ensure t :pin melpa
+  :config (add-to-list 'auto-mode-alist '("\\.json" . json-mode)))
+
+
+
+;; NPM integration
+(use-package npm-mode :ensure t :pin melpa)
+
+;;; General keymap.
+(when (fboundp 'general-create-definer)
+  (progn
+    ;; TIDE
+    (general-define-key
+     :keymaps 'js2-mode-map
+     :prefix "C-c"
+     "m" '(:ignore t :which-key "js2"))
+    (general-define-key
+     :keymaps 'js2-mode-map
+     :prefix "C-c m"
+    ;; NPM
+    "n" (general-simulate-key "C-c n" :name npm)
+     )
+    ;; JSON
+    (general-define-key
+     :keymaps 'json-mode-map
+     :prefix "C-c"
+     "m" '(:ignore t :which-key "json"))
+    (general-define-key
+     :keymaps 'json-mode-map
+     :prefix "C-c m"
+     ;;;
+     "p" 'jsons-print-path
+     "f" 'json-mode-beautify
+    )
+    ))
+
+
+
+;;;
+(defconst agelmacs/layer/js2 t)
+;;; EOF.
