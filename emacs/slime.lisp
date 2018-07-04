19b4350ac56cc0ab51b40df8400a9090c5ff5aa9
jonghyouk yun <ageldama@gmail.com>
2018-07-05 00:04:56 +0900

    --wip-- [skip ci]

---
+++ emacs/slime.lisp
@@ -0,0 +1,7 @@
+(use-package slime :ensure t :pin melpa
+  :config  (setq inferior-lisp-program
+                 (expand-file-name
+                  "~/local/sbcl-1.4.9-x86-64-linux/run-sbcl.sh"))
+  (setq slime-contribs '(slime-fancy )))
+
+
