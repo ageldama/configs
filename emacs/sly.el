8f089998eee40fe710e6e2334e71be21390ba34a
Jonghyouk Yun <ageldama@gmail.com>
2018-08-12 00:10:08 +0900

    ...

--- emacs/sly.el
+++ emacs/sly.el
@@ -1,6 +1,5 @@
 (use-package sly :ensure t :pin melpa
   :config  (setq inferior-lisp-program
-                 (expand-file-name
-                  "~/local/sbcl-1.4.9-x86-64-linux/run-sbcl.sh")))
+                 (expand-file-name "~/local/sbcl/run-sbcl.sh")))
 
 
