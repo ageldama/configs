5bd925333a681f8a5e3e7bd1595044d4ef38ff1f
Jonghyouk Yun <ageldama@gmail.com>
2018-08-12 22:59:42 +0900

    ...

--- emacs/perl5.el
+++ emacs/perl5.el
@@ -1,6 +1,7 @@
 ;; use cperl-mode instead of perl-mode
 (setq auto-mode-alist (rassq-delete-all 'perl-mode auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.\\(p\\([lm]\\)\\)\\'" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.t" . cperl-mode))
 
 (setq interpreter-mode-alist (rassq-delete-all 'perl-mode interpreter-mode-alist))
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
