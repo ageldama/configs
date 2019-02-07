905d611e90f85ab98583b783784e06c4721c957e
jonghyouk yun <ageldama@gmail.com>
2019-02-07 16:43:39 +0900

    refactor langsup.

--- emacs/example-dot-emacs.el
+++ emacs/example-dot-emacs.el
@@ -1,4 +1,4 @@
-(setq load-layer-base-path (expand-file-name "~/P/configs/emacs/"))
+(setq langsup-base-path (expand-file-name "~/P/configs/emacs/"))
 (load-file (expand-file-name "~/P/configs/emacs/dot-emacs-2019"))
 
 ;;;EOF.
