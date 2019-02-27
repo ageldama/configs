847b5e626c673d17b32ec40452f6a3bfd89a2f12
Jong-Hyouk Yun <ageldama@gmail.com>
2019-02-28 02:12:13 +0900

    --wip-- [skip ci]

--- emacs/examples/c-c++-gtags/GTAGS_dot-dir-locals.el
+++ emacs/examples/c-c++-gtags/GTAGS_dot-dir-locals.el
@@ -1,7 +1,7 @@
 ((nil . (
 				 (eval . (let ((root (projectile-project-root)))
 									 (message "PROJECT-BUILD-PATH: %S" root)
-									 (let ((build-dir (concat root "../build-cpp_scratchpad/")))
+									 (let ((build-dir (concat root "../build-TODO"))) ;; TODO!
 										 (setq-local project-build-path build-dir)
 										 (setq-local flycheck-clang-tidy-build-path build-dir))))
 				 )))
