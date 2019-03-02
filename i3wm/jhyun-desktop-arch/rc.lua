692ae693e4e3c41fa840c1d46c657b9ad466f3f9
Jong-Hyouk Yun <ageldama@gmail.com>
2019-03-03 03:45:39 +0900

    --wip-- [skip ci]

--- i3wm/jhyun-desktop-arch/rc.lua
+++ i3wm/jhyun-desktop-arch/rc.lua
@@ -659,4 +659,5 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+beautiful.font = 'sans 10'
 awful.spawn('wg.pl')
