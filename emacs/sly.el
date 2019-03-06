78059c50f3d9a44179a985fab25d55786d45e285
Jong-Hyouk Yun <ageldama@gmail.com>
2019-03-07 03:29:15 +0900

    --wip-- [skip ci]

--- emacs/sly.el
+++ emacs/sly.el
@@ -1,5 +1,8 @@
+(defun sly-no-company-mode ()
+  (company-mode -1))
+
 (use-package sly :ensure t :pin melpa
-  :config  (setq inferior-lisp-program (expand-file-name "~/local/sbcl/run-sbcl.sh")
-                 sly-ignore-protocol-mismatches t))
-
-
+  :config (setq inferior-lisp-program (expand-file-name "~/local/sbcl/run-sbcl.sh")
+                sly-ignore-protocol-mismatches t)
+  (add-hook 'sly-mode-hook #'sly-no-company-mode)
+  (add-hook 'sly-db-hook #'sly-no-company-mode))
