4d03a3077c2fc32a675c5b9256af4a1dbfcc53d1
Jong-Hyouk Yun <ageldama@gmail.com>
2019-03-10 20:18:57 +0900

    --wip-- [skip ci]

--- emacs/install-go-tools.pl
+++ emacs/install-go-tools.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/env perl
 
-foreach my $i (qw(
+foreach my $i (qw<
   golang.org/x/tools/...
   github.com/rogpeppe/godef
-  github.com/golang/lint/golint
+  golang.org/x/lint/golint
   github.com/alecthomas/gometalinter
   github.com/mdempsky/gocode
   github.com/godoctor/godoctor
   github.com/derekparker/delve/cmd/dlv
   github.com/stretchr/testify/assert
-)) {
+  github.com/client9/misspell/cmd/misspell
+  github.com/securego/gosec/cmd/gosec/...
+>) {
   print $i;
   print qx(go get -u -v ${i});
 }
