4c7db765e97a179984afbc2f3f2b5f689578c7b7
Jong-Hyouk Yun <ageldama@gmail.com>
2019-04-27 18:38:29 +0900

    --wip-- [skip ci]

---
+++ emacs/auctex.el
@@ -0,0 +1,3 @@
+
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
--- emacs/geiser.el
+++ emacs/geiser.el
@@ -1,2 +1,2 @@
 (use-package geiser :ensure t :pin melpa
-  :config (setq geiser-active-implementations '(guile racket)))
+  :config (setq geiser-active-implementations '(racket guile)))
