279a0b9de0725008132c145c834ced38eeeb9cee
Jong-Hyouk Yun <ageldama@gmail.com>
2019-10-12 11:02:33 +0900

    --wip-- [skip ci]

--- i3wm/jhyun-desktop-arch/rc.lua
+++ i3wm/jhyun-desktop-arch/rc.lua
@@ -117,6 +117,8 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Keyboard map indicator and switcher
 mykeyboardlayout = awful.widget.keyboardlayout()
 
+local cpu_widget = require("awesome-wm-widgets.cpu-widget.cpu-widget")
+
 -- {{{ Wibar
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
@@ -224,6 +226,7 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
+            cpu_widget(),
             mytextclock,
             s.mylayoutbox,
         },
