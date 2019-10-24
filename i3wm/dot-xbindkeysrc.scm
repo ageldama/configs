1ab65b959225338aa6221930baed0bae96e6eeed
Jong-Hyouk Yun <ageldama@gmail.com>
2019-10-25 08:53:20 +0900

    --wip-- [skip ci]

---
+++ i3wm/dot-xbindkeysrc.scm
@@ -0,0 +1,42 @@
+;;; EX-G pro 유무선 하이스펙 트랙볼 마우스 M-XPT1MRBK
+
+;;;;EXTRA FUNCTIONS: Enable numlock, scrolllock or capslock usage
+;;(set-numlock! #t)
+;;(set-scrolllock! #t)
+;;(set-capslock! #t)
+
+;;;;; Scheme API reference
+;;;;
+;; Optional modifier state:
+;; (set-numlock! #f or #t)
+;; (set-scrolllock! #f or #t)
+;; (set-capslock! #f or #t)
+;; 
+;; Shell command key:
+;; (xbindkey key "foo-bar-command [args]")
+;; (xbindkey '(modifier* key) "foo-bar-command [args]")
+;; 
+;; Scheme function key:
+;; (xbindkey-function key function-name-or-lambda-function)
+;; (xbindkey-function '(modifier* key) function-name-or-lambda-function)
+;; 
+;; Other functions:
+;; (remove-xbindkey key)
+;; (run-command "foo-bar-command [args]")
+;; (grab-all-keys)
+;; (ungrab-all-keys)
+;; (remove-all-keys)
+;; (debug)
+
+
+;; Examples of commands:
+
+;; (xbindkey '(control shift q) "xbindkeys_show")
+
+
+(xbindkey '("b:10") "xdotool key Next")
+(xbindkey '("b:11") "xdotool key Prior")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; End of xbindkeys guile configuration ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
