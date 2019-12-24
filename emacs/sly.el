3a931e7fea0b39ce0be3924ac19656b12fbb02d2
Jong-Hyouk Yun <ageldama@gmail.com>
2019-12-24 23:19:56 +0900

    --wip-- [skip ci]

--- emacs/sly.el
+++ emacs/sly.el
@@ -6,7 +6,7 @@
   (company-mode -1))
 
 (use-package sly :ensure t :pin melpa
-  :config (setq inferior-lisp-program (expand-file-name "~/local/sbcl/run-sbcl.sh")
+  :config (setq inferior-lisp-program (expand-file-name "/usr/bin/sbcl")
                 sly-ignore-protocol-mismatches t)
   (add-hook 'sly-mode-hook #'sly-no-company-mode)
   (add-hook 'sly-db-hook #'sly-no-company-mode))
