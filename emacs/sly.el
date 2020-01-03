f32025f4d0b2a197e260b3925a4cad6f6d3a334c
Jong-Hyouk Yun <ageldama@gmail.com>
2020-01-03 14:22:18 +0900

    --wip-- [skip ci]

--- emacs/sly.el
+++ emacs/sly.el
@@ -1,12 +1,17 @@
 ;;; Sly + Company integration is unstable,
-;;; (ex: C-g during company completion, Emacs hangs)
+;;; (NOT ANYMORE! `C-g' during `company-mode' completion, Emacs hangs)
 ;;;
-;;; Use C-M-i or M-Tab instead.
+;;; Use `C-M-i' or `M-Tab' instead.
 (defun sly-no-company-mode ()
   (company-mode -1))
 
 (use-package sly :ensure t :pin melpa
   :config (setq inferior-lisp-program (expand-file-name "/usr/bin/sbcl")
                 sly-ignore-protocol-mismatches t)
-  (add-hook 'sly-mode-hook #'sly-no-company-mode)
-  (add-hook 'sly-db-hook #'sly-no-company-mode))
+  (dolist (i '(sly-mode-hook sly-db-hook))
+    (add-hook i (lambda () (company-mode +1))))
+  ;;(add-hook 'sly-mode-hook #'sly-no-company-mode)
+  ;;(add-hook 'sly-db-hook #'sly-no-company-mode)
+  )
+
+(bind-company-local-key 'sly-mrepl-mode-hook (kbd "<C-tab>"))
