1a1d028d50366221a5d59870d281cd4533033f07
Jong-Hyouk Yun <ageldama@gmail.com>
2020-01-05 19:06:54 +0900

    --wip-- [skip ci]

---
+++ xscreensaver-toggle.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature qw(say);
+
+package main;
+
+my $pid = `pgrep -u \`id -u\` xscreensaver`;
+chomp $pid;
+
+if ($pid) {
+  qx[xscreensaver-command -exit];
+  qx[notify-send 'xscreensaver has killed'];
+} else {
+  my $pid = fork();
+  if ($pid == 0) {
+    qx[notify-send 'xscreensaver has started'];
+    exec('xscreensaver', '-no-splash');
+  }
+}
+
+
+
+
+
