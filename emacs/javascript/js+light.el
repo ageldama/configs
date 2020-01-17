5f12c997d64b18ee437a597c14a5da5723a14a58
jonghyouk yun <jhyun@promedius.ai>
2020-01-17 13:37:30 +0900

    --wip-- [skip ci]

--- emacs/javascript/js+light.el
+++ emacs/javascript/js+light.el
@@ -1,5 +1,26 @@
 (use-package add-node-modules-path :ensure t :pin melpa)
 
+
+;;; https://gist.github.com/CodyReichert/9dbc8bd2a104780b64891d8736682cea
+(use-package web-mode :ensure t :pin melpa)
+(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
+(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+
+;; (setq-default flycheck-disabled-checkers
+;;               (append flycheck-disabled-checkers
+;;                       '(javascript-jshint json-jsonlist)))
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+(defun web-mode-init-prettier-hook ()
+  (add-node-modules-path)
+  (prettier-js-mode))
+
+(add-hook 'web-mode-hook  'web-mode-init-prettier-hook)
+
+
+
+
 (require 'flycheck)
 (add-hook 'js-mode-hook #'add-node-modules-path)
 ;;(flycheck-add-mode 'javascript-eslint 'js-mode)
