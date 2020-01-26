b843e347be6d7ab594e90b242c87fdb158a8a409
Jong-Hyouk Yun <ageldama@gmail.com>
2020-01-26 18:34:54 +0900

    --wip-- [skip ci]

--- emacs/c++.el
+++ emacs/c++.el
@@ -31,59 +31,6 @@
   (flycheck-add-next-checker 'c/c++-clang-tidy 'c/c++-gcc)
   )
 
-;;; RTags
-(use-package rtags :ensure t :pin melpa
-  :config
-  (progn (setq rtags-autostart-diagnostics nil)
-         (setq rtags-completions-enabled t)))
-
-(setq rtags-verify-protocol-version nil)
-
-(use-package company-rtags :ensure t :pin melpa)
-
-
-(defun rtags-able? ()
-  (interactive)
-  (let ((fn (buffer-file-name)))
-    (not (s-blank?
-          (with-temp-buffer
-            (rtags-call-rc "-P" fn :noerror t)
-            (buffer-string))))))
-
-
-;;; RTags + ElDoc
-
-;;; eldoc
-(use-package eldoc :ensure t :pin melpa :diminish eldoc-mode)
-
-;;; Rtags + Eldoc:
-;; https://github.com/Andersbakken/rtags/issues/987
-(defun fontify-string (str mode)
-  "Return STR fontified according to MODE."
-  (with-temp-buffer
-    (insert str)
-    (delay-mode-hooks (funcall mode))
-    (font-lock-default-function mode)
-    (font-lock-default-fontify-region
-     (point-min) (point-max) nil)
-    (buffer-string)))
-
-(defun rtags-eldoc-function ()
-  (let ((summary (rtags-get-summary-text)))
-    (and summary
-         (fontify-string
-          (replace-regexp-in-string
-           "{[^}]*$" ""
-           (mapconcat
-            (lambda (str) (if (= 0 (length str)) "//" (string-trim str)))
-            (split-string summary "\r?\n")
-            " "))
-          major-mode))))
-
-(defun rtags-eldoc-mode ()
-  (interactive)
-  (setq-local eldoc-documentation-function #'rtags-eldoc-function)
-  (eldoc-mode 1))
 
 
 
@@ -115,6 +62,26 @@
 (use-package counsel-gtags :ensure t :pin melpa)
 
 
+;;; irony
+(use-package irony :ensure t :pin melpa)
+
+(use-package company-irony :ensure t :pin melpa
+  :config (add-to-list 'company-backends 'company-irony))
+
+(use-package irony-eldoc :ensure t :pin melpa)
+
+
+(add-hook 'irony-mode-hook (lambda ()
+                             (irony-cdb-autosetup-compile-options)
+                             (irony-cdb-json-add-compile-commands-path
+                              (projectile-project-root)
+                              (s-concat (getenv "BUILD_DIR")
+                                        "/compile_commands.json"))
+                             (irony-eldoc +1)
+                             (company-mode +1)))
+
+
+
 ;;; flycheck gcc/clang fixes
 (defvar my-c-c++-touched nil)
 
@@ -144,18 +111,11 @@
   (counsel-gtags-mode))
 
 
-(defun my-c-c++-rtags-hook ()
-  (require 'company)
-  (push 'company-rtags company-backends)
-  (rtags-start-process-unless-running)
-  (setq flycheck-clang-tidy-build-path (my-c-c++-build-dir)))
   
 
 (defun my-c-c++-mode-hook ()
   (interactive)
-  (if (rtags-able?)
-      (my-c-c++-rtags-hook)
-    (my-c-c++-gtags-hook))
+  (my-c-c++-gtags-hook)
   (unless my-c-c++-touched
     (run-with-timer 0.5 nil #'flycheck-c/c++-setup))
   (flycheck-mode)
@@ -163,6 +123,8 @@
   (c-c++-bind-key-map)
   ;; rmsbolt
   (setq-local rmsbolt-default-directory (my-c-c++-build-dir))
+  ;; irony
+  (irony-mode +1)
   )
 
 
@@ -181,13 +143,6 @@
 (add-hook 'c++-mode-local-vars-hook 'my-c-c++-mode-hook)
 
 
-(defun my-c-c++-eldoc ()
-  (interactive)
-  (when (rtags-able?)
-    (rtags-eldoc-mode)))
-
-(add-hook 'c-mode-hook 'my-c-c++-eldoc)
-(add-hook 'c++-mode-hook 'my-c-c++-eldoc)
 
 
 
@@ -209,40 +164,10 @@
     "g !" 'flycheck-c/c++-setup
     ))
 
-(defun rtags-local-defs ()
-  (message "Using RTags")
-  (define-key c-mode-base-map (kbd "M-.") #'rtags-find-symbol-at-point)
-  (define-key c-mode-base-map (kbd "M-,") #'rtags-location-stack-back)
-  (define-key c-mode-base-map (kbd "M-?") #'rtags-find-references-at-point)
-  ;;
-  (my-local-leader-def :keymaps 'c-mode-base-map
-    "` !" 'my-c-c++-mode-reset
-    "` b" 'c-c++-rmsbolt-this-or-off
-    
-    "?" 'rtags-print-symbol-info
-    "." 'rtags-find-symbol-at-point
-    "," 'rtags-location-stack-back
-    ">" 'rtags-find-references-at-point
-    ";" 'rtags-find-file
-    "v" 'rtags-find-virtuals-at-point
-    "[" 'rtags-previous-match
-    "]" 'rtags-next-match
-    "!" 'rtags-fix-fixit-at-point
-    "i" 'rtags-imenu
-    "d" 'rtags-diagnostics
-    "D" 'rtags-dependency-tree
-
-    "t" '(:ignore t :which-key "rtags")
-    "t s" 'rtags-find-symbol
-    "t r" 'rtags-find-references
-    "t R" 'rtags-references-tree
-    ))
 
 (defun c-c++-bind-key-map ()
   (when (fboundp 'general-create-definer)
-    (if (rtags-able?)
-        (rtags-local-defs)
-      (gtags-local-defs))))
+    (gtags-local-defs)))
 
 
 ;;; EOF
