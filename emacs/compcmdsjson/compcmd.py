72316847aab022e10cd9cfd75133cc01679e91a5
Jong-Hyouk Yun <ageldama@gmail.com>
2020-06-02 16:50:24 +0900

    --wip-- [skip ci]

---
+++ emacs/compcmdsjson/compcmd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import shlex
+import json
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit(1)
+
+    comp_cmds_json_fn = sys.argv[1]
+    d_src_fn = sys.argv[2]
+
+    with open(comp_cmds_json_fn, 'r') as in_f:
+        j = json.load(in_f)
+
+        for i in j:
+            if i['file'] == d_src_fn:
+                print(i['command'])
+                sys.exit(0)
+
+    sys.exit(1)  # Not found.
+
+        
