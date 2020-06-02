45c27662f80af13961e536988489b0188e8309b9
Jong-Hyouk Yun <ageldama@gmail.com>
2020-06-02 17:25:33 +0900

    --wip-- [skip ci]

--- emacs/compcmdsjson/compcmd.py
+++ emacs/compcmdsjson/compcmd.py
@@ -11,14 +11,32 @@ if __name__ == '__main__':
     comp_cmds_json_fn = sys.argv[1]
     d_src_fn = sys.argv[2]
 
+    rmsbolty = False
+    if len(sys.argv) >= 4 and sys.argv[3] == '--no-c-and-o':
+        rmsbolty = True
+    # print(rmsbolty)
+
     with open(comp_cmds_json_fn, 'r') as in_f:
         j = json.load(in_f)
 
         for i in j:
             if i['file'] == d_src_fn:
                 cmd = i['command']
-                parsed = shlex.split(cmd)
-                print(parsed)
+                if rmsbolty:
+                    parsed = shlex.split(cmd)
+                    skip = False
+                    coll = []
+                    for part in shlex.split(cmd):
+                        if part == '-c' or part == '-o':
+                            skip = True
+                        else:
+                            if skip:
+                                skip = False
+                            else:
+                                coll.append(part)
+                    print(shlex.join(coll))
+                else:
+                    print(cmd)    
                 sys.exit(0)
 
     sys.exit(1)  # Not found.
