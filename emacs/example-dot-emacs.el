d10dee263c14ef31da1058cc9b58f6c8cfdc4401
jonghyouk yun <ageldama@gmail.com>
2021-05-31 05:15:02 +0900

    fonts.

--- emacs/example-dot-emacs.el
+++ emacs/example-dot-emacs.el
@@ -1,4 +1,10 @@
 (setq langsup-base-path (expand-file-name "~/P/configs/emacs/"))
 (load-file (expand-file-name "~/P/configs/emacs/dot-emacs-2021.el"))
 
+(progn
+  (setq *ageldama/font-fixed-en* "DejaVu Sans Mono" 
+        *ageldama/font-fixed-ko* "나눔고딕코딩" )
+  (my-set-fixed-fonts))
+
+
 ;;;EOF.
