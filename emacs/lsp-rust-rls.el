7b5deb2607bd92d4658288b49645770172318a3a
jonghyouk yun <ageldama@gmail.com>
2021-06-08 04:15:52 +0900

    Tue Jun  8 04:16:03 AM KST 2021

--- emacs/lsp-rust-rls.el
+++ emacs/lsp-rust-rls.el
@@ -6,6 +6,8 @@
 (use-package cargo :ensure t :pin melpa
   :config (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
+(diminish 'cargo-minor-mode)
+
 (use-package flycheck-rust :ensure t :pin melpa
   :config (with-eval-after-load 'rust-mode
             (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
