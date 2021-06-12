fc44740f91ae4402c10dbf1bb3f1341d5e47c646
jonghyouk yun <ageldama@gmail.com>
2021-06-12 19:57:25 +0900

    --wip-- [skip ci]

---
+++ void-linux/inst.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+xbps-install vim tmux mc dialog \
+	dbus polkit elogind \
+	avahi avahi-utils nss-mdns \
+	samba \
+	xorg xorg-apps xorg-fonts xorg-input-drivers xorg-server ttf-bitstream-vera \
+	xinit xset xterm twm xfce4-panel xfwm4 Thunar thunar-volman \
+	openbox obconf gmrun x2x-git \
+	hsetroot feh xbindkeys rofi dunst notify-osd redshift-gtk \
+	git tig base-devel cmake ninja meson ctags gdb \
+	emacs-gtk3 clang-analyzer clang-tools-extra \
+	firefox mpv vlc mplayer telegram-desktop mcomix curl wget \
+	pulseaudio pavucontrol xscreensaver xmodmap \
+	fzf exa fd ripgrep zsh zsh-syntax-highlighting \
+	noto-fonts-cjk noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-emoji \
+	fonts-droid-ttf fonts-nanum-ttf fonts-nanum-ttf-extra \
+	dejavu-fonts-ttf font-hack-ttf font-sourcecodepro \
+	qbittorrent qbittorrent-nox kwrite \
+	fcitx5 fcitx5-configtool fcitx5-gtk+2 fcitx5-gtk+3 \
+	fcitx5-hangul fcitx5-hangul-icons fcitx5-icons \
+	fcitx5-qt fcitx5-qt5 fcitx5-qt6 \
+  xfce4-terminal gnome-screenshot gnome-keyring
+
+
+# dbus
+# logind
+# polkit
+# avahi
+# samba
+# mdns
+# pulseaudio
+	
+# oh-my-zsh
+	
