949d3be683953681b5fe9c0621618cac12aa9d6e
jonghyouk yun <ageldama@gmail.com>
2021-06-14 13:15:02 +0900

    --wip-- [skip ci]

--- dotfiles/_inst.sh
+++ dotfiles/_inst.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
+expand_tilde() {
+    tilde_less="${1#\~/}"
+    [ "$1" != "$tilde_less" ] && tilde_less="$HOME/$tilde_less"
+    printf '%s' "$tilde_less"
+}
+
 sdir="$1"
 ddir="$2"
 
-# TODO mkdir -pv ~/local/bin
+mkdir -pv ~/local/bin
 
 for f in $(/bin/ls -Aa $sdir); do
   if [[ "$f" == "." ]] || [[ "$f" == '..' ]]; then
     continue
   fi
 
-  fn=$PWD/local_bin/$f
-  if [[ -L "$fn" ]]; then
-    fn=$(readlink -f $fn)
+  sfn="$PWD/$1/$f"
+  if [[ -L "$sfn" ]]; then
+    sfn=$(readlink -f $sfn)
   fi
 
-  echo "$fn --> $ddir"
-  # TODO ln -sv "$fn" ~/local/bin
+  dfn="$ddir/$f"
+  dfn="$(expand_tilde $dfn)"
+  echo "    $sfn --> $dfn"
+  ln -sv "$sfn" "$dfn"
 done
 
