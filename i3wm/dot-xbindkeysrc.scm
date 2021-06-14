34464046b9c64d036814ac8cea9cf712923aa5a4
jonghyouk yun <ageldama@gmail.com>
2021-06-14 11:42:22 +0900

    --wip-- [skip ci]

--- i3wm/dot-xbindkeysrc.scm
+++ i3wm/dot-xbindkeysrc.scm
@@ -1,47 +1,37 @@
-;;; EX-G pro 유무선 하이스펙 트랙볼 마우스 M-XPT1MRBK
-
-;;;;EXTRA FUNCTIONS: Enable numlock, scrolllock or capslock usage
-;;(set-numlock! #t)
-;;(set-scrolllock! #t)
-;;(set-capslock! #t)
-
-;;;;; Scheme API reference
-;;;;
-;; Optional modifier state:
-;; (set-numlock! #f or #t)
-;; (set-scrolllock! #f or #t)
-;; (set-capslock! #f or #t)
-;; 
-;; Shell command key:
-;; (xbindkey key "foo-bar-command [args]")
-;; (xbindkey '(modifier* key) "foo-bar-command [args]")
-;; 
-;; Scheme function key:
-;; (xbindkey-function key function-name-or-lambda-function)
-;; (xbindkey-function '(modifier* key) function-name-or-lambda-function)
-;; 
-;; Other functions:
-;; (remove-xbindkey key)
-;; (run-command "foo-bar-command [args]")
-;; (grab-all-keys)
-;; (ungrab-all-keys)
-;; (remove-all-keys)
-;; (debug)
 
 
-;; Examples of commands:
-
-;; (xbindkey '(control shift q) "xbindkeys_show")
 
 
-(xbindkey '("b:6") "xdotool key Prior") ; wheel <-
-(xbindkey '("b:7") "xdotool key Next")  ; wheel ->
+(for-each (lambda (k+cmd) (xbindkey (car k+cmd) (cdr k+cmd)))
+  '(
+    ;; EX-G pro 유무선 하이스펙 트랙볼 마우스 M-XPT1MRBK
+    (("b:6")  . "xdotool key Prior") ; wheel <-
+    (("b:7")  . "xdotool key Next")  ; wheel ->
+    (("b:10") . "xdotool key Next")
+    (("b:11") . "xdotool key Prior")
+    (("b:12") . "xdotool key Next")
 
-(xbindkey '("b:10") "xdotool key Next")
-(xbindkey '("b:11") "xdotool key Prior")
+    ;;
+    ((Mod4 Return)  . "x-terminal-emulator")
+    ((Mod4 r)       . "gmrun")
+    ((Mod4 d)       . "rofi -show")
+    ((Mod4 Tab)     . "rofi -show windowcd")
+    ((Mod4 Escape)  . "xscreensaver-command -lock")
+    ((Mod4 p)       . "gnome-screenshot")
+    ((Mod4 Shift p) . "gnome-screenshot -i")
+    ((Mod4 e)       . "xdg-open $HOME")
+    ((Mod4 Shift e) . "x-terminal-emulator -e mc")
 
-(xbindkey '("b:12") "xdotool key Next")
+    ((Mod4 Shift w) . "hsetroot-solid.pl")
+    ((Mod4 w)       . "wg.pl")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; End of xbindkeys guile configuration ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ((Mod4 F8)      . "sh -c 'notify-send \"$(uptime) // $(date)\"'")
+    ((Mod4 F10)     . "pavucontrol")
+
+    ((Mod4 Shift F9) . "xscreensaver-toggle.pl; kill -USR1 $(pidof redshift)")
+  ))
+  ;;
+
+
+
+;;;EOF.
