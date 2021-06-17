f851cd9a01cdb54b84b2fa1284c9793927de5e64
jonghyouk yun <ageldama@gmail.com>
2021-06-18 07:01:24 +0900

    ...

--- emacs/lsp-cpp-clangd.el
+++ emacs/lsp-cpp-clangd.el
@@ -25,13 +25,13 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 
-;;(setq ;;gc-cons-threshold (* 100 1024 1024)
+(setq ;;gc-cons-threshold (* 100 1024 1024)
       ;;read-process-output-max (* 1024 1024)
       ;;treemacs-space-between-root-nodes nil
       ;;company-idle-delay 0.0
-      ;;company-minimum-prefix-length 1
+      company-minimum-prefix-length 1
       ;;lsp-idle-delay 0.1  ;; clangd is fast
-      ;;)
+      )
 
 
 (with-eval-after-load 'lsp-mode
