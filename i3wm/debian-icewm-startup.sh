956d759d2ac1eb1facedf68ff2cdbde93ef39888
jonghyouk yun <ageldama@gmail.com>
2021-07-11 21:01:45 +0900

    --wip-- [skip ci]

---
+++ i3wm/debian-icewm-startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#xsetroot -solid '#300020'
+
+~/local/bin/caps-to-ctrl.sh 
+
+xscreensaver -no-splash &
+
+(sleep 3s; xsetroot -solid '#a75480') &
+
+#xmessage  hey                                        
+
+
