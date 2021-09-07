b112d932cdf173b2b23e07cbc78fbb8e2f230f40
jonghyouk yun <ageldama@gmail.com>
2021-09-08 01:24:12 +0900

    --wip-- [skip ci]

--- emacs/dot-emacs-2021.el
+++ emacs/dot-emacs-2021.el
@@ -661,6 +661,7 @@ i.e. change right window to bottom, or change bottom window to right."
   (progn
     (global-set-key (kbd "C-'") 'avy-goto-char-timer)
     (global-set-key (kbd "C-:") 'avy-goto-char-2)
+    (global-set-key (kbd "C-c C-j") 'avy-resume)
     (global-set-key (kbd "M-g l") 'avy-goto-line)
     (global-set-key (kbd "M-g f") 'avy-goto-char)
     (global-set-key (kbd "M-g w") 'avy-goto-word-1)
