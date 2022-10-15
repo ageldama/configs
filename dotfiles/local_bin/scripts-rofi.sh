552c83619154f9de33934ffc77dc97669250836d
jonghyouk yun <ageldama@gmail.com>
2022-10-16 00:42:25 +0900

    --wip-- [skip ci]

--- dotfiles/local_bin/scripts-rofi.sh
+++ dotfiles/local_bin/scripts-rofi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_DIR=~/local/scripts/
-SEL=$(find $SCRIPT_DIR -executable -not -type d | rofi -dmenu -p "Script") && sh "${SEL}"
+SEL=$(find -L $SCRIPT_DIR -executable -not -type d | rofi -dmenu -p "Script") && sh "${SEL}"
 
