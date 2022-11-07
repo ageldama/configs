83dfed34422d4ac4a43d0de5cdc0da0390f941a6
jonghyouk yun <ageldama@gmail.com>
2022-11-07 16:12:45 +0900

    --wip-- [skip ci]

--- i3wm/wg.pl
+++ i3wm/wg.pl
@@ -11,7 +11,12 @@ use feature qw<say>;
   my $pick = $files[rand @files];
   say $pick;
 
-  system("pgrep -u $ENV{USER} gnome-shell && gsettings set org.gnome.desktop.background picture-uri ${pick} || feh --bg-fill ${pick}");
+
+  my $color_dark = `gsettings get org.gnome.desktop.interface color-scheme | grep -o \\\\-dark`;
+  chomp $color_dark;
+  #say $color_dark;
+
+  system("pgrep -u $ENV{USER} gnome-shell && gsettings set org.gnome.desktop.background picture-uri${color_dark} ${pick} || feh --bg-fill ${pick}");
 
   #
 =begin
