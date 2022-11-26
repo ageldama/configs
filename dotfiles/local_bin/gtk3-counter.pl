25e9f06b4e0ada2f28a2873544d0b9de94c6a698
jonghyouk yun <ageldama@gmail.com>
2022-11-26 21:40:21 +0900

    ...

--- dotfiles/local_bin/gtk3-counter.pl
+++ dotfiles/local_bin/gtk3-counter.pl
@@ -4,16 +4,13 @@ use warnings;
 use boolean;
 use Gtk3;
 
-Gtk3::init;
 
-my $window = Gtk3::Window->new('toplevel');
-$window->signal_connect(destroy => sub { Gtk3::main_quit; });
-
-my $vbox = Gtk3::Box->new('vertical', 2);
-$window->add($vbox);
-
-my $button_box = Gtk3::Box->new('horizontal', 2);
-$button_box->set_halign('center');
+sub quit_with_confirm {
+    my $parent = shift;
+    if(dlg_yes_no($parent, 'Really quit?')){
+        Gtk3::main_quit;
+    }
+}
 
 sub relabel_btn_count {
     my ($btn, $cnt) = @_;
@@ -28,9 +25,9 @@ sub config_btn_count {
 }
 
 sub dlg_yes_no {
-    my $msg = shift;
+    my ($parent, $msg) = @_;
     my $dialog = Gtk3::MessageDialog->new(
-        $window,
+        $parent,
         [qw( modal destroy-with-parent )],
         'question',
         'yes_no',
@@ -39,6 +36,18 @@ sub dlg_yes_no {
     $dialog->destroy;
     return $ans eq 'yes';
 }
+
+# main
+Gtk3::init;
+
+my $window = Gtk3::Window->new('toplevel');
+$window->signal_connect(destroy => sub { Gtk3::main_quit; });
+
+my $vbox = Gtk3::Box->new('vertical', 2);
+$window->add($vbox);
+
+my $button_box = Gtk3::Box->new('horizontal', 2);
+$button_box->set_halign('center');
 
 my $count = 0;
 my $btn_count = Gtk3::Button->new_with_label('foobar?');
@@ -54,7 +63,7 @@ relabel_btn_count($btn_count, $count);
 my $btn_decr = Gtk3::Button->new('--x');
 $btn_decr->signal_connect(
     clicked => sub {
-        if(dlg_yes_no('Decrease by 1?')){
+        if(dlg_yes_no($window, 'Decrease by 1?')){
             $count --;
             relabel_btn_count($btn_count, $count);
         }
@@ -64,7 +73,7 @@ $button_box->pack_start($btn_decr, false, false, 3);
 my $btn_reset = Gtk3::Button->new('x<-0');
 $btn_reset->signal_connect(
     clicked => sub {
-        if(dlg_yes_no('Reset?')){
+        if(dlg_yes_no($window, 'Reset?')){
             $count = 0;
             relabel_btn_count($btn_count, $count);
         }
@@ -72,7 +81,7 @@ $btn_reset->signal_connect(
 $button_box->pack_start($btn_reset, false, false, 3);
 
 my $btn_quit = Gtk3::Button->new('Quit');
-$btn_quit->signal_connect(clicked => sub { Gtk3::main_quit;});
+$btn_quit->signal_connect(clicked => sub { quit_with_confirm($window);});
 $button_box->pack_start($btn_quit, false, false, 3);
 
 $vbox->pack_start($button_box, false, false, 3);
