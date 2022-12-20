abcdc4c0fa331517830dbf643b15154c4e5fe608
jonghyouk yun <ageldama@gmail.com>
2022-12-20 10:02:35 +0900

    --wip-- [skip ci]

--- pa-get-vol.sh
+++ pa-get-vol.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-get_volume() {
-  pacmd list-sinks | grep -A15 '* index' | perl -ne'/^\s+volume:.+\s+(\d+%)/ && print $1'
-
-}
-
-get_muted() {
-  pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }'
-}
-
 get_sink_name() {
-  pacmd list-sinks | grep '* index' -A1 |awk '/name:/{print $2}'
+  pactl info | perl -ne '/default sink:\s+(.+)/i && print $1'
+}
+
+get_sink_info() {
+    NAME=$(get_sink_name)
+    pactl list sinks | grep "Name: $NAME" -A15
+}
+
+get_volume() {
+   get_sink_info | perl -ne '/^\s+volume:.+\s+(\d+%)/i && print $1'
+}
+
+get_muted_yesno() {
+  get_sink_info | perl -ne '/mute:\W+(\w+)/i && print $1'
 }
 
 SINK_NAME=$(get_sink_name)
 VOL=$(get_volume)
-MUTED=$(get_muted)
+MUTED=$(get_muted_yesno)
 MUTED_2=$(test "$MUTED" = "yes" && echo 'Muted' || echo 'Unmuted')
 
 echo "$SINK_NAME : $MUTED_2 / $VOL"
