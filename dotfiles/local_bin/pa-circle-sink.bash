de4394c08f45f237486eb1142d8db3fe0723f5e2
jonghyouk yun <ageldama@gmail.com>
2022-12-20 10:19:14 +0900

    --wip-- [skip ci]

--- dotfiles/local_bin/pa-circle-sink.bash
+++ dotfiles/local_bin/pa-circle-sink.bash
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+get_default_sink() {
+    pactl info | perl -ne '/default sink:\s+(.+)/i && print $1'
+}
+
+get_sinks() {
+    pactl list sinks | perl -ne '/^\s+Name:\s+(.+)/ && print $1 . "\n"'
+}
+
+# TODO handle(empty-list)
+
+SINKS=$(get_sinks)
+#declare -a SINKS
+
+for i in $SINKS
+do
+    echo "S: $i"
+done
+
+
+XXX=<<'EOF'
 new_sink=$(pacmd list-sinks | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
 
 echo "Setting default sink to: $new_sink";
@@ -14,4 +34,4 @@ pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
 done
 
 pacmd list-sinks | grep '* index' -A1 > /dev/stderr
-
+EOF
