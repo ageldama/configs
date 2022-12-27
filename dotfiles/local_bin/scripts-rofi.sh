e9928b14dfd576361aecd57b2bf1cedc03f2894e
jonghyouk yun <ageldama@gmail.com>
2022-12-27 23:07:48 +0900

    --wip-- [skip ci]

--- dotfiles/local_bin/scripts-rofi.sh
+++ dotfiles/local_bin/scripts-rofi.sh
@@ -2,6 +2,8 @@
 SCRIPT_DIR=~/local/scripts/
 
 FIND=find
+
+NO_FREEBSD=<<'EO_CASE'
 OSNAME=$(uname)
 
 case $OSNAME in
@@ -9,7 +11,7 @@ case $OSNAME in
     FIND=/usr/local/bin/gfind
     ;;
 esac
-
+EO_CASE
 #echo $FIND
 
 SEL=$($FIND -L $SCRIPT_DIR -executable -not -type d | rofi -dmenu -p "Script") && sh "${SEL}"
