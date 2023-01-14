7701353f719ff1169dbffe49181b85ea96f13b5f
jonghyouk yun <ageldama@gmail.com>
2023-01-15 00:47:39 +0900

    --wip-- [skip ci]

---
+++ dotfiles/preds/urxvt-ext.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+if(-r '/etc/os-release'){
+  open my $fh, '<', '/etc/os-release' or die;
+  while(<$fh>){
+    # opensuse은 패키지 있어서 생략하기:
+    exit 1 if m/opensuse/;
+  }
+  close $fh;
+}
+
+# debian은 아마 내 dotfiles해야 할듯:
+exit 0;
