1530d2ad23d969eb37abe12cc6e39655603f3da5
jhyun@rpi3b <ageldama@gmail.com>
2023-01-30 11:48:18 +0900

    --wip-- [skip ci]

---
+++ emacs/xemacs-init.el
@@ -0,0 +1,16 @@
+(setq user-full-name    "Jong-Hyouk Yun")
+(setq user-mail-address "ageldama@gmail.com")
+
+
+(require 'un-define)
+
+(set-language-environment     "English")
+(prefer-coding-system         'utf-8)
+;;(set-selection-coding-system  'utf-8)
+(set-default-coding-systems   'utf-8)
+(set-terminal-coding-system   'utf-8)
+(set-keyboard-coding-system   'utf-8)
+(setq                         locale-coding-system 'utf-8)
+
+
+(setq default-input-method   "korean-hangul")
