38c09a5578bed0d6b828038855367477a9aae437
jonghyouk yun <ageldama@gmail.com>
2023-02-01 12:54:03 +0900

    --wip-- [skip ci]

--- emacs/geiser.el
+++ emacs/geiser.el
@@ -2,5 +2,5 @@
   ;;:config (setq geiser-active-implementations '(racket guile))
   )
 
-;;(use-package geiser-guile :ensure t :pin melpa)
+(use-package geiser-guile :ensure t :pin melpa)
 (use-package geiser-racket :ensure t :pin melpa)
