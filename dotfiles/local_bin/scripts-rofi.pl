3c52174d638204e14d2ca7056f66fcfd8f6d3c84
jonghyouk yun <ageldama@gmail.com>
2023-02-14 06:18:34 +0900

    --wip-- [skip ci]

--- dotfiles/local_bin/scripts-rofi.pl
+++ dotfiles/local_bin/scripts-rofi.pl
@@ -6,16 +6,118 @@ use strict;
 use warnings;
 
 use DBI;
+use DDP;
 
 
 sub open_or_create {
   my $db_path = shift;
 
-  my $dbh = DBI->connect("dbi:SQLite:dbname=$db_path","","");
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$db_path", '', '',
+                         { RaiseError => 1, AutoCommit => 0, });
+
+  $dbh->{sqlite_allow_multiple_statements} = 1;
+
+  $dbh->do(<<'EO_SQL');
+create table if not exists hist (
+  path    text primary key,
+  cnt     integer default 0 not null,
+  mtime   integer default 0 not null
+);
+
+create index if not exists hist_ix_cnt
+    on hist (cnt);
+
+create index if not exists hist_ix_mtime
+    on hist (mtime);
+EO_SQL
+
+  $dbh->commit;
+  $dbh->{sqlite_allow_multiple_statements} = 0;
 
   return bless {
     dbh => $dbh,
    };
+}
+
+
+sub update_paths {
+  my ($self, $scripts) = @_;
+  my $dbh = $self->{dbh};
+
+  my $sth = $dbh->prepare_cached(<<'EO_SQL');
+insert into hist (path, cnt, mtime)
+    values (?, 0, current_timestamp)
+on conflict (path)
+    do update set mtime = current_timestamp
+EO_SQL
+
+  foreach my $s (@$scripts) {
+    $sth->execute($s);
+  }
+
+  $dbh->commit;
+}
+
+
+sub list_sorted {
+  my ($self, $base_dir) = @_;
+  my $dbh = $self->{dbh};
+
+  my $sth = $dbh->prepare_cached(<<'EO_SQL');
+select path from hist
+    where path like ? || '%'
+    order by mtime desc, cnt desc
+EO_SQL
+
+  $sth->bind_param(1, $base_dir);
+  $sth->execute;
+
+  my @result = ();
+
+  while ( my @row = $sth->fetchrow_array ) {
+    push @result, $row[0];
+  }
+
+  $dbh->rollback;
+
+  return \@result;
+}
+
+
+sub update_sel {
+  my ($self, $sel) = @_;
+  my $dbh = $self->{dbh};
+
+  my $sth = $dbh->prepare_cached(<<'EO_SQL');
+update hist
+    set cnt = cnt + 1
+        ,mtime = current_timestamp
+    where path = ?
+EO_SQL
+
+  $sth->execute($sel);
+  $dbh->commit;
+}
+
+
+sub get_recent {
+  my ($self, $base_dir) = @_;
+  my $dbh = $self->{dbh};
+
+  my $sth = $dbh->prepare_cached(<<'EO_SQL');
+select path from hist
+    where path like ? || '%'
+    order by mtime desc
+    limit 1
+EO_SQL
+
+  $sth->bind_param(1, $base_dir);
+  $sth->execute;
+
+  while ( my @row = $sth->fetchrow_array ) {
+    $dbh->rollback;
+    return $row[0];
+  }
 }
 
 
@@ -40,7 +142,6 @@ getopts('psre', \%opts);
 
 use constant SCRIPT_DIR => "$ENV{HOME}/local/scripts";
 use constant HISTORY_DB => "$ENV{HOME}/.scripts-rofi.sqlite3";
-use constant SEL_SAVE => "$ENV{HOME}/.scripts-rofi.sh";
 
 
 sub HELP_MESSAGE {
@@ -49,8 +150,8 @@ sub HELP_MESSAGE {
 List content of [${ \SCRIPT_DIR }] and ask to select:
 
   -p : print selection
-  -s : save selection to [${ \SEL_SAVE }]
-  -r : rerun saved selection [${ \SEL_SAVE }]
+  -s : save selection
+  -r : rerun last saved selection
   -e : execute selection
 
 Exiting.
@@ -59,12 +160,27 @@ EO_HELP
   exit 0;
 }
 
+
+my $history_db = ScriptRofi::HistoryDB::open_or_create(HISTORY_DB);
+#p $history_db;
+
+
 # rerun?
 if($opts{r}){
-  if(-x SEL_SAVE){
-    exit system(SEL_SAVE);
+  my $saved = $history_db->get_recent(SCRIPT_DIR);
+
+  if($saved){
+    if($opts{p}){
+      print "$saved\n";
+    }
+
+    if($opts{e}){
+      exit system($saved);
+    }
+
+    exit 0;  # fallback
   }else{
-    die 'No saved selection (exiting): ' . SEL_SAVE;
+    die 'No saved selection (exiting)';
   }
 }
 
@@ -85,14 +201,18 @@ find(
    );
 
 
-my $history_db = ScriptRofi::HistoryDB::open_or_create(HISTORY_DB);
+
+$history_db->update_paths(\@scripts);
+
+my $scripts_sorted = $history_db->list_sorted(SCRIPT_DIR);
+
 
 my $pid = open2(my $chld_out, my $chld_in,
                 'rofi -dmenu -p "Select a script to run"'
                ) or die;
 # say "rofi-pid: $pid";
 
-print $chld_in "$_\n" foreach @scripts;
+print $chld_in "$_\n" foreach @$scripts_sorted;
 
 close($chld_in) or die;
 
@@ -113,16 +233,14 @@ chomp $stdout;
 close($chld_out) or die;
 
 if($opts{s}){
-  open(my $fh_out, '>', SEL_SAVE) or die;
-  print $fh_out "$stdout\n";
-  close($fh_out);
-
-  chmod(0700, SEL_SAVE) or die;
+  $history_db->update_sel($stdout);
 }
 
 if($opts{p}){
   print "$stdout\n";
-}elsif($opts{e}){
+}
+
+if($opts{e}){
   system($stdout);
 }
 
