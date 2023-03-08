03f0c747b5eaa7dd476eb693650869a2b7df3f56
jonghyouk yun <ageldama@gmail.com>
2023-03-08 13:42:58 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/pwr/poweroff.sh
+++ dotfiles/local_scripts/pwr/poweroff.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+zenity --question --text='power-ff?' || exit -1
+
 pgrep systemd && systemctl poweroff
 xterm -e sudo poweroff
--- dotfiles/local_scripts/pwr/reboot.sh
+++ dotfiles/local_scripts/pwr/reboot.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+zenity --question --text='reboot?' || exit -1
+
 pgrep systemd && systemctl reboot
 xterm -e sudo reboot
