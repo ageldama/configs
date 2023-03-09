2f166b12915635a6b7ed08896c23227b6d488624
jonghyouk yun <ageldama@gmail.com>
2023-03-10 02:43:15 +0900

    --wip-- [skip ci]

--- dot-my-gtk-tim-btns.pl
+++ dot-my-gtk-tim-btns.pl
@@ -4,43 +4,58 @@ my $cmds = [
   {
     name => 'gtk3-counter',
     cmd => 'gtk3-counter.pl',
-    font => 'normal 32px serif',
+    font => 'normal 28px serif',
+    fg => 'red',
     },
 
     {
         name => 'calibre',
         cmd => 'calibre',
         font => 'normal 28px serif',
+        fg => 'blue',
     },
 
     {
         name => 'qjoypad',
         cmd => 'qjoypad',
         font => 'normal 28px serif',
+        fg => 'green',
     },
 
     {
         name => 'goldendict',
         cmd => 'goldendict',
         font => 'normal 28px serif',
+        fg => 'gold',
+        bg => 'darkgrey',
     },
 
     {
         name => 'fm4u',
         cmd => 'x-terminal-emulator -e fm4u.pl',
         font => 'normal 24px serif',
+        fg => 'cyan',
     },
 
     {
         name => 'pavucontrol',
         cmd => 'pavucontrol',
         font => 'normal 20px serif',
+        fg => 'green',
     },
 
     {
         name => 'onboard',
         cmd => 'onboard',
         font => 'normal 20px serif',
+        fg => 'brown',
+    },
+
+    {
+        name => 'blueman',
+        cmd => 'blueman-manager',
+        font => 'normal 20px serif',
+        fg => 'blue',
     },
 
     {
