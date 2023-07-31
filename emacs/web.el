44fede33515dc04a785e6d29d7aefd5e00955737
jonghyouk yun <ageldama@gmail.com>
2023-07-31 13:41:22 +0900

    web-mode: php

--- emacs/web.el
+++ emacs/web.el
@@ -1,6 +1,8 @@
 (use-package web-mode :ensure t :pin melpa)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 (setq web-mode-engines-alist
-      '(("django" . "\\.jj2.html\\'")))
+      '(("blade"  . "\\.blade\\.")
+        ("django" . "\\.jj2.html\\'")))
