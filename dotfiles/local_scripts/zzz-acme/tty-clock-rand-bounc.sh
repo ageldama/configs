795044fc4ccc04c948292c5d4aba9e0704f80687
jonghyouk yun <ageldama@gmail.com>
2023-11-04 19:10:18 +0900

    tty-clock

--- dotfiles/local_scripts/zzz-acme/tty-clock-rand-bounc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-x-terminal-emulator -e tty-clock -s -r -C $(perl -e 'print int(rand(8))')
-
---
+++ dotfiles/local_scripts/zzz-acme/xterm-tty-clock.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+x-terminal-emulator -e ~/local/scripts/zzz-acme/tty-clock-rand-bounc.bash
+
