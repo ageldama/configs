cdc94d43ee47819258ed4d004af2cd57b6ee2260
jonghyouk yun <ageldama@gmail.com>
2024-02-03 00:05:27 +0900

    --wip-- [skip ci]

--- emacs/golang-light.el
+++ emacs/golang-light.el
@@ -1,7 +1,6 @@
-(use-package go-mode :ensure t :pin melpa
-  :config (add-hook 'go-mode-hook (lambda ()
-                                    (setq company-idle-delay 1.0)
-                                    (add-hook 'before-save-hook 'gofmt-before-save))))
+(use-package go-mode :ensure t :pin melpa)
+
+(use-package gotest :ensure t :pin melpa)
 
 (when (and (fboundp 'go-mode) (memq window-system '(mac ns)))
   (use-package exec-path-from-shell :ensure t :pin melpa)
@@ -9,19 +8,26 @@
   (exec-path-from-shell-copy-env "GOPATH"))
 
 
-(defhydra hudra-lang-go-light ()
+(defhydra hydra-lang-go-light ()
   "go-light"
+
+  ("r" go-run "run" :exit t)
+  ("t" go-test-current-test "t-curT" :exit t)
+  ("T" go-test-current-file "t-curF" :exit t)
+  
   ("f"   gofmt "fmt" :exit t)
+
   ("d"   godoc "doc" :exit t)
   ("M-d" godoc-at-point "doc-pt" :exit t)
+
   ("D"   godef-describe "desc" :exit t)
   ("+"   go-import-add "imp+" :exit t)
   ("."   godef-jump "jmp" :exit t)
 
   ("SPC" nil))
 
-   ;; "g"   'go-goto-map
-   ;; "u"   'go-guru-map
+;; "g"   'go-goto-map
+;; "u"   'go-guru-map
 
 
 (lang-mode-hydra-set 'go-mode-hook 'hydra-lang-go-light/body)
