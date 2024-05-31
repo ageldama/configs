46079032c2b64725a658b8f65c4d7fe788f84e66
jonghyouk yun <jhyun@vetching.cc>
2024-05-31 13:43:22 +0900

    --wip-- [skip ci]

--- emacs/python/elpy.el
+++ emacs/python/elpy.el
@@ -1,15 +1,3 @@
 
-(require 'direnv)
-
 (use-package elpy :ensure t :pin melpa
-  :config (add-hook 'python-mode-hook
-                    (lambda ()
-                      (message "venv=%s" (getenv "VIRTUAL_ENV"))
-                      (when (getenv "VIRTUAL_ENV")
-                        ;;(message "venv=%s" (getenv "VIRTUAL_ENV"))
-                        (pyvenv-activate (getenv "VIRTUAL_ENV")))
-                      (elpy-enable)))
-  ;;
-  (when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode)))
+  :config (add-hook 'python-mode-hook #'elpy-enable))
