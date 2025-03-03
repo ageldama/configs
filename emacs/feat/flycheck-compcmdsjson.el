bf9db22b9925cd84261064ba78f8ad797c5b4a0e
jonghyouk yun <ageldama@gmail.com>
2025-03-03 19:05:10 +0900

    compile_commands.json

--- emacs/feat/flycheck-compcmdsjson.el
+++ emacs/feat/flycheck-compcmdsjson.el
@@ -5,3 +5,12 @@
 
 (define-key c-mode-base-map (kbd "C-c ! C-f") 'flycheck-compcmdsjson/apply)
 (define-key c-mode-base-map (kbd "C-c ! C-M-f") 'flycheck-compcmdsjson/forget)
+
+
+;; TODO compile_commands.json 하위디렉토리에서 검색?
+
+;; TODO compile_commands.json / "file" 경로를 project-root으로
+;; normalize <= from-build-dir
+
+;; TODO compile_commands.json / incdir 경로이 rel-path이면
+;; project-root, build-dir등에서 normalize.
