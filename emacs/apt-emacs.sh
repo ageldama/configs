4de9a19ecb2633c0c653d56b678d8981bff89109
jonghyouk yun <ageldama@gmail.com>
2025-04-07 13:07:49 +0900

    --wip-- [skip ci]

---
+++ emacs/apt-emacs.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+DPKGS="
+emacs-lucid
+emacs-common-non-dfsg
+elpa-use-package
+elpa-f
+elpa-s
+elpa-org
+elpa-modus-themes
+elpa-hydra
+elpa-which-key
+elpa-smart-mode-line
+elpa-avy
+elpa-ace-window
+elpa-ace-window
+elpa-eshell-up
+elpa-wgrep
+elpa-expand-region
+elpa-undo-tree
+elpa-evil
+elpa-diminish
+elpa-exec-path-from-shell
+elpa-company
+elpa-editorconfig
+elpa-hl-todo
+elpa-markdown-mode
+elpa-projectile
+elpa-embark
+elpa-vimish-fold
+elpa-flycheck
+elpa-counsel
+elpa-yasnippet
+elpa-web-mode
+"
+
+DPKGS=$(echo $DPKGS | sed 's/\n//g')
+echo "INSTALL: $DPKGS ..."
+sudo apt install $DPKGS
+
+TODO=<<'EO_TODO'
+clang flycheck-doc elpa-flx elpa-imenu-list libtext-multimarkdown-perl pandoc
+org-mode-doc ditaa texlive-latex-extra texlive-fonts-recommended texinfo
+elpa-ag exuberant-ctags elpa-solarized-theme elpa-use-package-chords
+elpa-use-package-ensure-system-package elpa-bind-chord
+EO_TODO
+
