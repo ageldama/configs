b16b55803a454d1553cfdfa6d11a09e2b5734728
jonghyouk yun <ageldama@gmail.com>
2025-04-10 01:44:13 +0900

    counsel.

--- emacs/feat/counsel.el
+++ emacs/feat/counsel.el
@@ -8,13 +8,16 @@
                   enable-recursive-minibuffers  t
                   ivy-re-builders-alist
                   '(
-                    (swiper      . ivy--regex-plus)
+                    ;; (swiper      . ivy--regex-plus)
                     ;; (counsel-M-x . ivy--regex-plus)
-                    (t           . ivy--regex-fuzzy)
+                    ;; (t           . ivy--regex-fuzzy)
+                    (t           . ivy--regex-plus)
                     ))
+            (ivy-configure 'counsel-M-x :initial-input "") ;; ^... 지우기귀찮.
             (global-set-key "\C-s" 'swiper)
             (global-set-key (kbd "M-s s") 'swiper-thing-at-point)
             (global-set-key (kbd "C-c C-r") 'ivy-resume)
+            (global-set-key (kbd "M-s M-z") 'counsel-fzf)
             (global-set-key (kbd "<f6>") 'ivy-resume)
             (global-set-key (kbd "M-x") 'counsel-M-x)
             (global-set-key (kbd "C-x C-f") 'counsel-find-file)
