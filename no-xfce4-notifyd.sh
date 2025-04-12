628490ca7e04c2efad8f5e2bf56cc2f12802ca0c
jonghyouk yun <ageldama@gmail.com>
2025-04-12 17:34:49 +0900

    --wip-- [skip ci]

---
+++ no-xfce4-notifyd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+systemctl --user disable --now xfce4-notifyd
+sudo mv /usr/share/dbus-1/services/org.xfce.xfce4-notifyd.Notifications.service /usr/share/dbus-1/services/org.xfce.xfce4-notifyd.Notifications.service.disabled
