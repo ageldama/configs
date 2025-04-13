e3c153f90a7fb9df7c555d02b6a62046f03b4224
jonghyouk yun <ageldama@gmail.com>
2025-04-13 23:19:49 +0100

    --wip-- [skip ci]

---
+++ emacs/samples/example-dot-emacs.el
@@ -0,0 +1,18 @@
+
+(setq default-input-method  "korean-hangul"
+      user-full-name        "Jong-Hyouk Yun"
+      user-mail-address     "ageldama@gmail.com"
+      org-crypt-key         "ageldama@gmail.com"
+    )
+
+(cl-pushnew "~/P/configs/emacs" load-path :test #'equal)
+
+(require 'e-2025)
+
+(require 'ag-evil-collection)
+
+
+;;; TEMP
+
+
+
