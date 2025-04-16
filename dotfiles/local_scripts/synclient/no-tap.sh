f4156996ca3b8a863b519d12f55cb173c125b65b
jonghyouk yun <ageldama@gmail.com>
2025-04-16 22:27:13 +0900

    --wip-- [skip ci]

---
+++ dotfiles/local_scripts/synclient/no-tap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+synclient PalmDetect=1
+
+synclient TapButton1=0
+synclient TapButton2=0
---
+++ dotfiles/local_scripts/synclient/tap-to-click.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+synclient PalmDetect=1
+
+synclient TapButton1=1
+
+# (right button event for two finger tap here): 
+synclient TapButton2=3
