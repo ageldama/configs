2d5ff098b1f77ade84b1304dea8590df8cd7e92e
jonghyouk yun <ageldama@gmail.com>
2025-04-17 20:34:15 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/xscreensaver-demo-rand-fullscreen.bash
+++ dotfiles/local_scripts/xscreensaver-demo-rand-fullscreen.bash
@@ -3,46 +3,29 @@
 
 XSCREENSAVER_DIR=/usr/libexec/xscreensaver
 
+declare -a XSCREENSAVERS=()
 
-XSCREENSAVERS=(/usr/libexec/xscreensaver/abstractile
-/usr/libexec/xscreensaver/binaryhorizon
-/usr/libexec/xscreensaver/binaryring
-/usr/libexec/xscreensaver/cwaves
-/usr/libexec/xscreensaver/deco
-/usr/libexec/xscreensaver/distort
-/usr/libexec/xscreensaver/droste
-/usr/libexec/xscreensaver/fiberlamp
-/usr/libexec/xscreensaver/fuzzyflakes
-/usr/libexec/xscreensaver/galaxy
-/usr/libexec/xscreensaver/hexadrop
-/usr/libexec/xscreensaver/m6502
-/usr/libexec/xscreensaver/marbling
-/usr/libexec/xscreensaver/metaballs
-/usr/libexec/xscreensaver/penrose
-/usr/libexec/xscreensaver/popsquares
-/usr/libexec/xscreensaver/ripples
-/usr/libexec/xscreensaver/scooter
-/usr/libexec/xscreensaver/shadebobs
-/usr/libexec/xscreensaver/slidescreen
-/usr/libexec/xscreensaver/swirl
-/usr/libexec/xscreensaver/tessellimage
-/usr/libexec/xscreensaver/xlyapr)
+# xscreensaver-gl
+for DPKG in xscreensaver-data xscreensaver-data-extra xscreensaver-screensaver-bsod; do
+  S=$(dpkg -L ${DPKG} | grep -E "^${XSCREENSAVER_DIR}/")
+  XSCREENSAVERS+=(${S})
+done
 
 SELECTED_IDX=$[$RANDOM % ${#XSCREENSAVERS[@]}]
 SELECTED=${XSCREENSAVERS[$SELECTED_IDX]}
 
-echo "${SELECTED}"
+echo "SELECTED: ${SELECTED}"
 "${SELECTED}" &
 PID=$!
-echo $PID
+echo "PID: ${PID}"
 
 sleep 0.5s
-WID=$(wmctrl -lp | awk "\$3 == $PID { print \$1 }")
-echo $WID
+WID=$(wmctrl -lp | awk "\$3 == ${PID} { print \$1 }")
+echo "WID: ${WID}"
 
-wmctrl -i -r $WID -b add,fullscreen
+wmctrl -i -r ${WID} -b add,fullscreen
 
-trap "kill $PID" EXIT
+trap "kill ${PID}" EXIT
 
-wait $PID
+wait ${PID}
 
