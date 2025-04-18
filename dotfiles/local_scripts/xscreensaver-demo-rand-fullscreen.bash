bb161a556f1ab8ba2eb67ad2cb6eb1e82a565177
jonghyouk yun <ageldama@gmail.com>
2025-04-18 11:47:42 +0900

    !

--- dotfiles/local_scripts/xscreensaver-demo-rand-fullscreen.bash
+++ dotfiles/local_scripts/xscreensaver-demo-rand-fullscreen.bash
@@ -7,23 +7,45 @@ declare -a XSCREENSAVERS=()
 
 # xscreensaver-gl
 for DPKG in xscreensaver-data xscreensaver-data-extra xscreensaver-screensaver-bsod; do
-  S=$(dpkg -L ${DPKG} | grep -E "^${XSCREENSAVER_DIR}/")
-  XSCREENSAVERS+=(${S})
+    mapfile -t S < <(dpkg -L ${DPKG} | grep -E "^${XSCREENSAVER_DIR}/")
+    # echo "LEN: ${#S[@]}"
+    XSCREENSAVERS+=( "${S[@]}" )
 done
+# echo "XSCREENSAVERS: ${#XSCREENSAVERS[@]}"
+
+# FIXME:
+# xscreensaver-auth
+# xscreensaver-getimage
+# xscreensaver-getimage-file
+# xscreensaver-getimage-video
+# xscreensaver-gfx
+# xscreensaver-gl-visual
+# xscreensaver-systemd
+# xscreensaver-text
+
+
+
 
 SELECTED_IDX=$((RANDOM % ${#XSCREENSAVERS[@]}))
 SELECTED=${XSCREENSAVERS[$SELECTED_IDX]}
 
-echo "SELECTED: ${SELECTED}"
+echo "SELECTED: [${SELECTED}]"
 "${SELECTED}" &
 PID=$!
 echo "PID: ${PID}"
 
-sleep 0.5s
-WID=$(wmctrl -lp | awk "\$3 == ${PID} { print \$1 }")
-echo "WID: ${WID}"
+while true; do
+    WID=$(wmctrl -lp | awk "\$3 == ${PID} { print \$1 }")
+    if [[ "${WID}" -ne "" ]]; then
+        echo "WID: ${WID}"
 
-wmctrl -i -r "${WID}" -b add,fullscreen
+        if wmctrl -i -r "${WID}" -b add,fullscreen; then
+            break
+        fi
+    fi
+
+    sleep 0.1s
+done
 
 function TRAP_KILL_CHILD () {
   kill "${PID}"
