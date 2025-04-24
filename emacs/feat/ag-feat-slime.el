41b4d626c6995a0b87ff69c2578cd7201c7cd1a9
jonghyouk yun <ageldama@gmail.com>
2025-04-24 12:04:03 +0900

    !

--- emacs/feat/ag-feat-slime.el
+++ emacs/feat/ag-feat-slime.el
@@ -10,7 +10,17 @@
 
 
 (use-package slime :ensure t :pin melpa
-  :config  (setq slime-contribs '(slime-fancy)))
+  :config  (setq slime-contribs '(slime-fancy))
+
+  (add-hook 'slime-repl-mode-hook
+            (lambda ()
+              (when (featurep 'counsel)
+                (local-set-key (kbd "C-c ,")
+                               'counsel-slime-repl-history)
+                (message "REPL History: C-c ,")
+                )))
+
+  )
 
 
 (provide 'ag-feat-slime)
