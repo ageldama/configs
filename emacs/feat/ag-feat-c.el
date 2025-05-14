43d71363c7b37f71080d90ea7040b9c2828fc50a
jonghyouk yun <ageldama@gmail.com>
2025-05-15 02:21:12 +0900

    !

--- emacs/feat/ag-feat-c.el
+++ emacs/feat/ag-feat-c.el
@@ -1,16 +1,5 @@
 
 
-(add-hook 'c-mode-hook
-          (lambda ()
-            ;; (when (featurep 'hideshow)
-            ;;   (hs-minor-mode +1))
-            (when (featurep 'ggtags)
-              (when (featurep 'eldoc)
-                (setq-local eldoc-documentation-function
-                            #'ggtags-eldoc-function)
-                (eldoc-mode +1))
-              (ggtags-mode +1))))
-
 
 (defhydra hydra-lang-c ()
   "c"
@@ -20,7 +9,20 @@
   ("SPC" nil))
 
 
-(lang-mode-hydra-set 'c-mode-hook 'hydra-lang-c/body)
+(dolist (mode '(c-mode-hook c++-mode-hook))
+  (add-hook mode
+            (lambda ()
+              ;; (when (featurep 'hideshow)
+              ;;   (hs-minor-mode +1))
+              (when (featurep 'ggtags)
+                (when (featurep 'eldoc)
+                  (setq-local eldoc-documentation-function
+                              #'ggtags-eldoc-function)
+                  (eldoc-mode +1))
+                (ggtags-mode +1))))
+
+  (lang-mode-hydra-set mode 'hydra-lang-c/body)
+  )
 
 
 (provide 'ag-feat-c)
