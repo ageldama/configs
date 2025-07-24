e5a82fac8be276fe8c525e3e3d8501b972333b2b
jonghyouk yun <ageldama@gmail.com>
2025-07-25 08:49:12 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/mount-smb-freebsd-samsung--home2.sh
+++ dotfiles/local_scripts/mount-smb-freebsd-samsung--home2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 SMB_USER=aamadleg
+SMB_HOST=freebsd-samsung.local
+SMB_IP=$(getent hosts $SMB_HOST | head -n1 | awk '{print $1}')
 
-read -s -p "SMB Password (for ${SMB_USER}): " SMB_PASSWD
 
-sudo mount -t cifs -o vers=3.0,user=${SMB_USER},password=${SMB_PASSWD} //freebsd-samsung.local/home2 /home/aamadleg/mnts/freebsd-samsung--home2/
+read -s -p "SMB Password (for ${SMB_USER} @ ${SMB_IP} == ${SMB_HOST}): " SMB_PASSWD
+
+sudo mount -t cifs -o vers=3.0,user=${SMB_USER},password=${SMB_PASSWD} //${SMB_IP}/home2 ${HOME}/mnts/freebsd-samsung--home2/
 
 
 
