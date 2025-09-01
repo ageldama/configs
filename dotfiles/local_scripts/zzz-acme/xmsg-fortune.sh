0d727265f30ae5cbec16b2262436f5f8a7a7a2ce
jonghyouk yun <ageldama@gmail.com>
2025-09-02 00:57:45 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
+++ dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
@@ -2,12 +2,10 @@
 
 FORTUNE=$(basename $(find /usr/share/games/fortunes -maxdepth 1 -name '*.dat' -printf '%f\n' | shuf -n1) .dat)
 
-xmessage "$(fortune -a $FORTUNE)
-
-
-
-*** from: $FORTUNE ***" \
+xmessage "$(fortune -a $FORTUNE)" \
+  -title "fortune: $FORTUNE" \
   -default okay -center \
   -fn 12x24 \
   -bg black -fg '#ffbf00'
 
+# -xrm 'xmessage.title: "Title"'
