1c7f8d396e401e3424e091e12ad0487858dd6a52
jonghyouk yun <ageldama@gmail.com>
2025-09-02 00:46:54 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
+++ dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-xmessage "$(fortune -a `find /usr/share/games/fortunes -maxdepth 1  -name '*.dat' -printf '%f\n' | sed s/.dat$//g | shuf -n1`)" \
+
+FORTUNE=$(basename $(find /usr/share/games/fortunes -maxdepth 1 -name '*.dat' -printf '%f\n' | shuf -n1) .dat)
+
+xmessage "$(fortune -a $FORTUNE)
+
+
+
+*** from: $FORTUNE ***" \
   -default okay -center \
   -fn 12x24 \
   -bg black -fg '#ffbf00'
