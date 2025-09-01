3fe92114259d1c2be2c791b0237c862982d6a2a7
jonghyouk yun <ageldama@gmail.com>
2025-09-02 00:34:43 +0900

    --wip-- [skip ci]

---
+++ dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xmessage "$(fortune -a `find /usr/share/games/fortunes -maxdepth 1  -name '*.dat' -printf '%f\n' | sed s/.dat$//g | shuf | head -n1`)" \
+  -default okay -center \
+  -fn 12x24 \
+  -bg black -fg '#ffbf00'
+
