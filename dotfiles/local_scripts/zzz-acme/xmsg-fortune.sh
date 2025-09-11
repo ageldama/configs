b633f4531c1299c6f8d921416c33139883ff4f99
jonghyouk yun <ageldama@gmail.com>
2025-09-12 01:12:01 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
+++ dotfiles/local_scripts/zzz-acme/xmsg-fortune.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-FORTUNE=$(basename $(find /usr/share/games/fortunes -maxdepth 1 -name '*.dat' -printf '%f\n' | shuf -n1) .dat)
+DIR=/usr/share/games/fortunes
+FIND=find
+SHUF=shuf
+BASENAME=basename
+FORTUNE_PREFIX=
 
-xmessage "$(fortune -a $FORTUNE)" \
+# FreeBSD: /usr/local/share/games/fortune
+if [ "$(uname)" = "FreeBSD" ]; then
+    DIR=/usr/local/share/games/fortune
+    FIND=gfind
+    SHUF=gshuf
+    BASENAME=gbasename
+    FORTUNE_PREFIX="${DIR}/"
+fi
+
+FORTUNE=$($BASENAME $($FIND $DIR -maxdepth 1 -name '*.dat' -printf '%f\n' | $SHUF | head -n1) .dat)
+
+echo $FORTUNE
+
+xmessage "$(fortune -a ${FORTUNE_PREFIX}${FORTUNE})" \
   -title "fortune: $FORTUNE" \
   -default okay -center \
   -fn 12x24 \
