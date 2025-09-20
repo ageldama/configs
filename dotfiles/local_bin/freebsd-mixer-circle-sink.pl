439999e8060ec797fa264a804fa3812b9f97e5f8
jonghyouk yun <ageldama@gmail.com>
2025-09-20 15:34:21 +0900

    --wip-- [skip ci]

---
+++ dotfiles/local_bin/freebsd-mixer-circle-sink.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my @pcm_lines = grep(/^pcm.+\:/, qx<mixer -a>);
+
+my %pcms = ();
+
+foreach my $l (@pcm_lines) {
+  $l =~ m/^(?<name>pcm.+)\:/;
+  my $pcm = $+{name};
+
+  $l =~ m/(?<default>\(default\))/;
+  $pcms{$pcm} = defined $+{default};
+}
+
+# print Dumper(\%pcms);
+my $cur = 0;
+while (1) {
+  foreach my $pcm (sort(keys %pcms)) {
+    if ($cur) {
+      my $stdout = qx<mixer -d $pcm>;
+      qx<notify-send "$stdout">;
+      goto BYE;
+    }
+
+    $cur = 1 if $pcms{$pcm};
+  }
+}
+BYE:
