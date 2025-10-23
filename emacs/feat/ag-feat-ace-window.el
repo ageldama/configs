33f48f7b7307b604c13284eceb9489581140d86b
jonghyouk yun <ageldama@gmail.com>
2025-10-24 02:12:55 +0900

    M-o

--- emacs/feat/ag-feat-ace-window.el
+++ emacs/feat/ag-feat-ace-window.el
@@ -2,6 +2,7 @@
 (use-package ace-window :ensure t :pin melpa
   :config
   (global-set-key (kbd "C-x o") 'ace-window)
+  (global-set-key (kbd "M-o") 'ace-window)
   (global-set-key (kbd "C-x O") 'aw-show-dispatch-help)
   )
 
