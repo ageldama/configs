99145c5dc1e2f86819a66b2ebf933c8911fe87c9
jonghyouk yun <ageldama@gmail.com>
2025-10-25 16:51:59 +0900

    no more duplicates :sob:

--- dotfiles/local_bin/find-dups-less-nested.pl
+++ dotfiles/local_bin/find-dups-less-nested.pl
@@ -5,10 +5,25 @@ use strict;
 use warnings;
 
 use File::Find;
+use Getopt::Long::Descriptive;
+
+
+my ($opt, $usage) = describe_options(
+  '%c %o',
+  [ 'delete|D' => 'Delete dupe-files' ],
+ );
+
+
+if(!$opt || $opt->{help}){
+  print $usage->text;
+  exit;
+}
+
+my $really_delete = defined $opt->{delete};
 
 
 my %winners = ();
-
+my @to_be_deleteds = ();
 
 find({
   wanted => sub {
@@ -23,11 +38,13 @@ find({
         # stored-wins: PRINT current
         print "# longer:\t$stored_fname\n";
         print "\t$fname\n";
+        push @to_be_deleteds, $fname;
       }else{
         # challenger-wins: PRINT stored, UPDATE stored
         print "# longer:\t$fname\n";
         print "\t\t$stored_fname\n";
         $winners{$sname} = $fname;
+        push @to_be_deleteds, $stored_fname;
       }
     }else{
       $winners{$sname} = $fname;
@@ -35,3 +52,15 @@ find({
   },
   follow => 1,
 }, qw(.));
+
+
+if($really_delete){
+  print "===> DELETING:\n";
+  foreach my $del (@to_be_deleteds) {
+    print "DEL:\t$del\n";
+    unlink $del or warn "FAIL($!)\n";
+  }
+  print "===> DELETED.\n";
+}else{
+  print "===> DRY-RUN HAS FINISHED.\n";
+}
