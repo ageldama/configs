a243a4df6338216f10df3df92e7def4e893b296e
jonghyouk yun <ageldama@gmail.com>
2025-11-04 23:53:56 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/zzz-acme/xclock-digital-mini.sh
+++ dotfiles/local_scripts/zzz-acme/xclock-digital-mini.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 xclock -digital -update 1 -twentyfour -render \
        -strftime '%H:%M:%S' -bg '#222222' -fg '#fb7c00' \
-       -face 'DSEG7 Classic:style=Bold Italic:size=30'
+       -face 'DSEG7 Classic:style=Bold Italic:size=16' &
+
+PID=$!
+
+sleep 1s
+
+icesh -p $PID borderless move 0 0
+
 
