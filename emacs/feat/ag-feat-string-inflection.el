ed4a497f454b024e055e730542a560c8965403dc
jonghyouk yun <ageldama@gmail.com>
2025-11-17 06:26:49 +0900

    --wip-- [skip ci]

--- emacs/feat/ag-feat-string-inflection.el
+++ emacs/feat/ag-feat-string-inflection.el
@@ -1,9 +1,9 @@
 
-(use-package string-inflection  :ensure t :pin melpa
-  :config
-  (when (fboundp 'defhydra)
-    (eval '(defhydra hydra-string-inflection ()
-             "
+(use-package string-inflection  :ensure t :pin melpa)
+
+(when (fboundp 'defhydra)
+  (eval '(defhydra hydra-string-inflection ()
+           "
 string-inflection:^^
 ----------------------------------------------------------
 _c_: 'fooBar' lower-camelcase
@@ -13,18 +13,17 @@ _k_: 'foo-bar' kebab
 _u_: 'foo_bar' underscore
 _U_: 'FOO_BAR' upcase
 "
-             ("c" string-inflection-lower-camelcase :exit t)
-             ("C" string-inflection-camelcase :exit t)
-             ("p" string-inflection-capital-underscore :exit t)
-             ("k" string-inflection-kebab-case :exit t)
-             ("u" string-inflection-underscore :exit t)
-             ("U" string-inflection-upcase :exit t)
-             ("SPC" nil)))
+           ("c" string-inflection-lower-camelcase :exit t)
+           ("C" string-inflection-camelcase :exit t)
+           ("p" string-inflection-capital-underscore :exit t)
+           ("k" string-inflection-kebab-case :exit t)
+           ("u" string-inflection-underscore :exit t)
+           ("U" string-inflection-upcase :exit t)
+           ("SPC" nil)))
 
-    (require 'ag-hydra--main)
-    (add-to-list 'hydra-mini/++extras
-                 '("M-i" hydra-string-inflection/body "infl."))
-
-    ))
+  (require 'ag-hydra--main)
+  (add-to-list 'hydra-mini/++extras
+               '("M-i" hydra-string-inflection/body "infl."))
+  )
 
 (provide 'ag-feat-string-inflection)
