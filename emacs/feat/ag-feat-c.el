23cad2143c44dcad30ed498884ce7acfba146a45
jonghyouk yun <ageldama@gmail.com>
2025-11-17 03:31:26 +0900

    fix

--- emacs/feat/ag-feat-c.el
+++ emacs/feat/ag-feat-c.el
@@ -1,14 +1,5 @@
 
 
-(when (fboundp 'defhydra)
-  (eval '(defhydra hydra-lang-c ()
-           "c"
-
-           ("\\" ff-find-other-file "hdr<->src" :exit t)
-
-           ("SPC" nil))))
-
-
 (dolist (mode '(c-mode-hook c++-mode-hook))
   (add-hook mode
             (lambda ()
@@ -20,10 +11,22 @@
                               #'ggtags-eldoc-function)
                   (eldoc-mode +1))
                 (ggtags-mode +1))))
+  )
+
+(when (fboundp 'defhydra)
+  (eval '(defhydra hydra-lang-c ()
+           "c"
+
+           ("\\" ff-find-other-file "hdr<->src" :exit t)
+
+           ("SPC" nil)))
 
   (require 'ag-lang-mode)
-  (lang-mode-hydra-set mode 'hydra-lang-c/body)
-  )
+  (lang-mode-hydra-set 'c-mode-hook 'hydra-lang-c/body)
+  (lang-mode-hydra-set 'c++-mode-hook 'hydra-lang-c/body))
+
+
+
 
 
 (provide 'ag-feat-c)
