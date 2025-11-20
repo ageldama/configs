1c59b781224095a3bf4076488521aa6a2f03396c
jonghyouk yun <ageldama@gmail.com>
2025-11-20 22:03:54 +0900

    --wip-- [skip ci]

--- dotfiles/bashrc.local.d/dev.bash
+++ dotfiles/bashrc.local.d/dev.bash
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-
 if [[ -x ~/.local/bin/mise ]]; then
     eval "$(~/.local/bin/mise activate bash)"
 fi
 
+if [[ -d ~/.local/share/mise/shims ]]; then
+    export PATH=~/.local/share/mise/shims:$PATH
+fi
 
-
-eval "$(direnv hook bash)"
-
-
-#if [[ -d ~/.local/share/mise/shims ]]; then
-#   export PATH=~/.local/share/mise/shims:$PATH
-#fi
+which direnv > /dev/null && eval "$(direnv hook bash)"
 
 
 
