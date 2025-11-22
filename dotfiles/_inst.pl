90c997a3b1829a9e0f5cf4a1ef248b047cb1e7b3
jonghyouk yun <ageldama@gmail.com>
2025-11-22 12:25:16 +0900

    corelist

--- dotfiles/_inst.pl
+++ dotfiles/_inst.pl
@@ -7,51 +7,51 @@ use Cwd          qw(getcwd abs_path);
 use File::Path   qw(make_path);
 use File::Basename;
 use IO::Dir;
-use Path::ExpandTilde;        # no-builtin
-use Data::Dumper::OneLine;    # no-builtin
+use File::Glob   qw(bsd_glob);
+use Data::Dumper;
 
 # main:
 {
-    my $inst_or_uninst = shift;    # 'i' or 'u' for installing/uninstalling.
-    my $src_dir        = shift;
-    my $dst_dir        = expand_tilde shift;
+  local $Data::Dumper::Indent = 0;
+  local $Data::Dumper::Terse = 1;
 
-    # dst_Dir
+  my $inst_or_uninst = shift; # 'i' or 'u' for installing/uninstalling.
+  my $src_dir        = shift;
+  my $dst_dir        = bsd_glob shift;
+
+  # dst_Dir
+  if ( $inst_or_uninst eq 'i' ) {
+    my @created = make_path($dst_dir);
+    print "INSTALL: $dst_dir ... mkdir_p: " . Dumper( \@created ) . "\n";
+  } else {
+    print "UNINSTALL: $dst_dir ... (did nothing)\n";
+  }
+
+  tie my %dir, 'IO::Dir', $src_dir;
+  foreach my $fn ( keys %dir ) {
+    next if $fn eq '.' or $fn eq '..';
+
+    # src_fn, dst_fn :
+    my $src_fn = "$src_dir/$fn";
+    my $dst_fn = "$dst_dir/$fn";
+
+    # readlink (...for real / as realink(1)) :
+    $src_fn = abs_path($src_fn);
+
+    #say Dumper([$src_fn, $dst_fn]);
+
+    # install -or- uninstall :
     if ( $inst_or_uninst eq 'i' ) {
-        my @created = make_path($dst_dir);
-        print "INSTALL: $dst_dir ... mkdir_p: " . Dumper( \@created ) . "\n";
+      print "INSTALL: $src_fn\t-->\t$dst_fn ... ";
+      if ( -e $dst_fn ) {
+        print "SKIPPING (existing)\n";
+      } else {
+        symlink( $src_fn, $dst_fn ) or warn "$! : $src_fn => $dst_fn";
+        print "SYMLINK\n";
+      }
+    } else {
+      my $unlinked = unlink $dst_fn;
+      print "UNINSTALL: $dst_fn ... $unlinked\n";
     }
-    else {
-        print "UNINSTALL: $dst_dir ... (did nothing)\n";
-    }
-
-    tie my %dir, 'IO::Dir', $src_dir;
-    foreach my $fn ( keys %dir ) {
-        next if $fn eq '.' or $fn eq '..';
-
-        # src_fn, dst_fn :
-        my $src_fn = "$src_dir/$fn";
-        my $dst_fn = "$dst_dir/$fn";
-
-        # readlink (...for real / as realink(1)) :
-        $src_fn = abs_path($src_fn);
-
-        #say Dumper([$src_fn, $dst_fn]);
-
-        # install -or- uninstall :
-        if ( $inst_or_uninst eq 'i' ) {
-            print "INSTALL: $src_fn\t-->\t$dst_fn ... ";
-            if ( -e $dst_fn ) {
-                print "SKIPPING (existing)\n";
-            }
-            else {
-                symlink( $src_fn, $dst_fn ) or warn "$! : $src_fn => $dst_fn";
-                print "SYMLINK\n";
-            }
-        }
-        else {
-            my $unlinked = unlink $dst_fn;
-            print "UNINSTALL: $dst_fn ... $unlinked\n";
-        }
-    }
+  }
 }
