f265a453cb3202ef62b3418528f0dd78a16f237d
jonghyouk yun <ageldama@gmail.com>
2025-12-03 02:02:54 +0900

    1

--- emacs/feat/ag-feat-slime.el
+++ emacs/feat/ag-feat-slime.el
@@ -1,27 +1,19 @@
-(let ((hyperspec-path
-       (glob-first-file "/usr/share/doc/hyperspec/"
-                        "/usr/local/share/doc/clisp-hyperspec/HyperSpec/"
-                        (expand-file-name "~/local/HyperSpec/"))))
-  (when hyperspec-path
-    (setq common-lisp-hyperspec-root hyperspec-path)))
-
-
-
-(setq inferior-lisp-program "ros -Q run")
-
 
 (use-package slime :ensure t :pin melpa
   :config  (setq slime-contribs '(slime-fancy))
 
-  (add-hook 'slime-repl-mode-hook
-            (lambda ()
-              (when (featurep 'counsel)
-                (local-set-key (kbd "C-c ,")
-                               'counsel-slime-repl-history)
-                (message "REPL History: C-c ,")
-                )))
+  ;; (add-hook 'slime-repl-mode-hook
+  ;;           (lambda ()
+  ;;             (when (featurep 'counsel)
+  ;;               (local-set-key (kbd "C-c ,")
+  ;;                              'counsel-slime-repl-history)
+  ;;               (message "REPL History: C-c ,")
+  ;;               )))
 
   )
+
+
+(require 'ag-feat-common-lisp)
 
 
 (provide 'ag-feat-slime)
--- emacs/feat/ag-feat-sly.el
+++ emacs/feat/ag-feat-sly.el
@@ -1,6 +1,4 @@
 
-
-
 (use-package sly :ensure t :pin melpa
   :config
   (sly-setup)
@@ -14,21 +12,7 @@
 
   )
 
-(let ((hyperspec-path
-       (glob-first-file "/usr/share/doc/hyperspec/"
-                        (expand-file-name "~/local/HyperSpec/"))))
-  (when hyperspec-path
-    (setq common-lisp-hyperspec-root hyperspec-path)))
-
-(require 'ag-reinit)
-(ag-reinit/add-as-interactive
- (setq inferior-lisp-program
-       (cond
-        ((executable-find "ros") "ros -Q run")
-        ((executable-find "sbcl") "sbcl")))
- (message "INF-LISP: %s\n"
-          inferior-lisp-program)
- )
+(require 'ag-feat-common-lisp)
 
 
 (provide 'ag-feat-sly)
