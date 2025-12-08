375ba08b9012ffc63e6443dd838369e854f6ce80
jonghyouk yun <ageldama@gmail.com>
2025-12-08 15:47:57 +0900

    xclock-mini

--- dotfiles/local_scripts/zzz-acme/xclock-digital-mini.sh
+++ dotfiles/local_scripts/zzz-acme/xclock-digital-mini.sh
@@ -7,6 +7,27 @@ PID=$!
 
 sleep 1s
 
-icesh -p $PID borderless move 0 0
+icesh -p $PID borderless bottom right sticky setLayer 12
 
 
+EOD=<<EOF
+
+sticky
+        Show the window on all workspaces.
+
+
+setLayer
+        Window layer
+        Named symbols of the domain Window layer (numeric range: 0-15):
+
+Desktop                (0)
+Below                  (2)
+Normal                 (4)
+OnTop                  (6)
+Dock                   (8)
+AboveDock             (10)
+Menu                  (12)
+Fullscreen            (14)
+AboveAll              (15)
+
+EOF
