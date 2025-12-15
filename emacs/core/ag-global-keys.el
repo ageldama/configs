268be43c56fceb105f22edd29b5378ed7873abbd
jonghyouk yun <ageldama@gmail.com>
2025-12-16 04:09:33 +0900

    C-f8

--- emacs/core/ag-global-keys.el
+++ emacs/core/ag-global-keys.el
@@ -9,15 +9,15 @@
    (evil-global-set-key 'normal (kbd "\\") 'do-lang-mode-hydra))
 
  ;; NOTE it overrides `suspend-emacs':
- (global-set-key (kbd "C-z") 'hydra-mini/body)
+ ;; (global-set-key (kbd "C-z") 'hydra-mini/body)
 
  ;; (global-set-key (kbd "C-c SPC") 'hydra-mini/body)
  ;; (global-set-key (kbd "C-c \\") 'do-lang-mode-hydra)
 
  ;; (global-set-key (kbd "C-c 9") 'do-lang-mode-hydra)
 
- ;; (global-set-key (kbd "C-<f8>") 'hydra-mini/body)
- ;; (global-set-key (kbd "C-<f9>") 'do-lang-mode-hydra)
+ (global-set-key (kbd "<f8>") 'hydra-mini/body)
+ (global-set-key (kbd "C-<f8>") 'do-lang-mode-hydra)
 
  (global-set-key (kbd "M-s d") 'find-name-dired)
  )
