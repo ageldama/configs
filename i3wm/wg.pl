7d7ddbc23f687d5d7ec6672bb814a10947bb5434
jonghyouk yun <ageldama@gmail.com>
2025-12-26 17:54:52 +0900

    --wip-- [skip ci]

--- i3wm/wg.pl
+++ i3wm/wg.pl
@@ -6,25 +6,32 @@ use feature qw<say>;
 #use Glib::Object::Introspection;
 
 {
-  my $pick = shift;
+    my $pick = shift;
 
-  if(!defined($pick)){
-    my @files = split /\n/, qx<find -L ~/P/wg/ -type f -not -path '*/\.git/*'>;
-    say scalar @files;
-    $pick = $files[rand @files];
-    say $pick;
-  }
+    if ( !defined($pick) ) {
+        my @files = split /\n/,
+          qx<find -L ~/P/wg/ -type f -not -path '*/\.git/*'>;
+        say scalar @files;
+        $pick = $files[ rand @files ] or die;
+        say $pick;
+    }
 
-  my $color_dark = `gsettings get org.gnome.desktop.interface color-scheme | grep -o \\\\-dark` || undef;
+    my $color_dark =
+`gsettings get org.gnome.desktop.interface color-scheme | grep -o \\\\-dark`
+      || undef;
 
-  if(defined $color_dark){
-    chomp $color_dark;
-    system("pgrep -u $ENV{USER} '^gnome-shell\$' && gsettings set org.gnome.desktop.background picture-uri${color_dark} ${pick} || feh --bg-fill ${pick}");
-  }else{
-    system("feh --bg-fill ${pick}");
-  }
+    if ( defined $color_dark ) {
+        chomp $color_dark;
+        system(
+"pgrep -u $ENV{USER} '^gnome-shell\$' && gsettings set org.gnome.desktop.background picture-uri${color_dark} \'${pick}\' || feh --bg-fill \'${pick}\'"
+        );
+    }
+    else {
+        system("feh --bg-fill \'${pick}\'");
+    }
 
-  #
+    #
+
 =begin
   Glib::Object::Introspection->setup (
     basename => 'Notify',
@@ -35,5 +42,7 @@ use feature qw<say>;
   my $n = Notify::Notification->new("", $s, "dialog-information");
   $n->show;
 =cut
+
 }
+
 #EOF
