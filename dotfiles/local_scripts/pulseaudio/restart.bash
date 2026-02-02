e50ae54c3b03a28217f7e3452a8ebbe7fbe5c871
jonghyouk yun <ageldama@gmail.com>
2026-02-02 18:15:21 +0900

    --wip-- [skip ci]

--- dotfiles/local_scripts/pulseaudio/restart.bash
+++ dotfiles/local_scripts/pulseaudio/restart.bash
@@ -1,2 +1,11 @@
-systemctl --user restart pulseaudio
+#!/usr/bin/env bash
+
+if systemctl --user | grep pulseaudio; then
+    echo SYSTEMD
+    systemctl --user restart pulseaudio
+else
+    echo MANUAL
+    pulseaudio -k
+    pulseaudio -D
+fi
 
