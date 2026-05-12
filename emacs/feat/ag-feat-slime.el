7cf21a2c510011c2a075c97c4972ae4e2acdb792
jonghyouk yun <ageldama@gmail.com>
2026-05-13 03:21:46 +0900

    --wip-- [skip ci]

--- emacs/feat/ag-feat-slime.el
+++ emacs/feat/ag-feat-slime.el
@@ -2,13 +2,13 @@
 (use-package slime :ensure t :pin melpa
   :config  (setq slime-contribs '(slime-fancy))
 
-  ;; (add-hook 'slime-repl-mode-hook
-  ;;           (lambda ()
-  ;;             (when (featurep 'counsel)
-  ;;               (local-set-key (kbd "C-c ,")
-  ;;                              'counsel-slime-repl-history)
-  ;;               (message "REPL History: C-c ,")
-  ;;               )))
+  (add-hook 'slime-repl-mode-hook
+            (lambda ()
+              (when (featurep 'counsel)
+                (local-set-key (kbd "C-c ,")
+                               'counsel-slime-repl-history)
+                (message "REPL History: C-c ,")
+                )))
 
   )
 
