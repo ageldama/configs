3abdfdd1e4f35b1827ad538ded668bd91f38ff3d
jonghyouk yun <ageldama@gmail.com>
2026-05-23 00:15:43 +0900

    --wip-- [skip ci]

--- dotfiles/bashrc.local.d/OPT/tcl.linkme.bash
+++ dotfiles/bashrc.local.d/OPT/tcl.linkme.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-DIR="${HOME}/local/tcl/lib"
+LOCAL_TCL_DIR="${HOME}/local/tcl"
 
 if [ -z "$TCLLIBPATH" ]; then
-    export TCLLIBPATH="$DIR"
+    export TCLLIBPATH="$LOCAL_TCL_DIR/lib"
 else
     export TCLLIBPATH="$TCLLIBPATH $DIR"
 fi
 
-export PATH=$PATH:$HOME/local/tcl/bin
+export PATH=$PATH:$LOCAL_TCL_DIR/bin
+export TCL9_0_TM_PATH=$LOCAL_TCL_DIR/tm
+
 
