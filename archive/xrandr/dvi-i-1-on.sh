f3b2be3c95eba92482ea59143c8b37457718f7d2
jonghyouk yun <ageldama@gmail.com>
2026-05-23 21:45:32 +0900

    byebye my memories.

--- archive/xrandr/dvi-i-1-on.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output DVI-I-1 --left-of DVI-D-0 --mode 1920x1080
--- archive/xrandr/off-vga-0-xrandr.sh
+++
@@ -1,9 +0,0 @@
-xrandr --output VGA-0 --off \
-    --output HDMI-0 --off \
-    --output LVDS --mode 1366x768
-
-xmodmap -e 'keycode 66 = Control_L'
-xmodmap -e 'clear Lock'
-xmodmap -e 'add Control = Control_L'
-
-
