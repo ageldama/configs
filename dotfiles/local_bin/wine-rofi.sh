a673d25dc8df4364c3171f4c44d02f0d30cf74bb
jonghyouk yun <ageldama@gmail.com>
2026-05-26 21:22:23 +0900

    done & done.

--- dotfiles/local_bin/wine-rofi.sh
+++ dotfiles/local_bin/wine-rofi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scripts-rofi -e -S ~/.wine/drive_c/users/ -D ~/.scripts-rofi.wine.hist -W wine -/ '\.lnk$' -p -i
+scripts-rofi -e -S ~/.wine/drive_c/users/ -D ~/.scripts-rofi.wine.hist -W wine -/ '\.lnk$' -p -i -s
