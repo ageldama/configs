3c8ffa7f6fa3bb22d7f4c86de4dc32d3d1e9891f
jonghyouk yun <ageldama@gmail.com>
2026-06-16 17:52:54 +0900

    for freebsd

--- dotfiles/local_scripts/pwr/poweroff.sh
+++ dotfiles/local_scripts/pwr/poweroff.sh
@@ -5,3 +5,5 @@ yesno.pl 'power-ff?' || exit -1
 xterm -e loginctl poweroff &
 pgrep systemd && systemctl poweroff &
 xterm -e sudo poweroff &
+
+wait
--- dotfiles/local_scripts/pwr/reboot.sh
+++ dotfiles/local_scripts/pwr/reboot.sh
@@ -5,3 +5,5 @@ yesno.pl 'reboot?' || exit -1
 xterm -e loginctl reboot &
 pgrep systemd && systemctl reboot &
 xterm -e sudo reboot &
+
+wait
