2f037ab4c99c06570ff98bc4e4a8a7da0c116a19
jonghyouk yun <ageldama@gmail.com>
2026-06-29 00:37:59 +0900

    --wip-- [skip ci]

---
+++ machines/freebsd-macbook-air/acpi-resume.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xmodmap -e 'keycode 94 = grave asciitilde'
+caps-to-ctrl.sh 3s &
+
+# ~/.acpi-resume.sh
+
---
+++ machines/freebsd-macbook-air/acpi-suspend.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export DISPLAY=:0
+export XAUTHORITY=$HOME/.Xauthority
+xscreensaver-command -lock
+
+
+# ~/.acpi-suspend.sh
