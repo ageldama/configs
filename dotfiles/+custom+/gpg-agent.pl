6946776999262749ca9400eb3950e2e41eae2b47
jonghyouk yun <ageldama@gmail.com>
2026-07-05 14:19:58 +0900

    gpg-agent + dotfiles

--- dotfiles/+custom+/gpg-agent.pl
+++ dotfiles/+custom+/gpg-agent.pl
@@ -4,7 +4,45 @@ exit 0 unless -f glob('~/.use-gpg-agent');
 use strict;
 use warnings;
 use Cwd qw(realpath);
+use Data::Dumper;
+
+
+sub grep_file_1st {
+  my ($fn, $regex) = @_;
+
+  open(my $fh, '<:encoding(UTF-8)', $fn) or die $!;
+  if(grep { /$regex/ } <$fh>){
+    close($fh);
+    return 1;
+  }
+
+  close($fh);
+  return 0;
+}
+
+sub one_of_executable {
+  for my $f (@_) {
+    return $f if -x $f;
+  }
+  die "No suitable executable: " . Dumper(\@_);
+}
+
+
 
 mkdir "$ENV{HOME}/.gnupg" or warn "mkdir; $!";
-#symlink realpath('../emacs/snippets'), "$ENV{HOME}/.emacs.d/snippets" or die "symlink: $!";
 
+my $fn = "$ENV{HOME}/.gnupg/gpg-agent.conf";
+
+exit 0 if grep_file_1st($fn, '^pinentry-program');
+
+
+
+
+my $pinentry = one_of_executable "/usr/local/bin/pinentry-gtk-2", "/usr/bin/pinenetry-gtk-2";
+# print STDERR $pinentry;
+
+open(my $fh, '>>', $fn) or die "Could not open '$fn': $!";
+print $fh "\npinentry-program $pinentry\n";
+close($fh);
+
+system("gpgconf --kill gpg-agent");
