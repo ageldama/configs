be39de3ed5c1782e86897dc6873a2b765e07f1e5
jonghyouk yun <ageldama@gmail.com>
2026-07-09 23:38:26 +0900

    dnd-from.tcl: multiple files

--- dotfiles/local_bin/dnd-from.tcl
+++ dotfiles/local_bin/dnd-from.tcl
@@ -6,21 +6,21 @@ if {[llength $argv] < 1} {
     exit 1
 }
 
-set file_path [file normalize [lindex $argv 0]]
+set file_paths [lmap fn $argv {file normalize $fn}]
+set file_paths [lmap path $file_paths {subst "file://$path"}]
+set file_paths [join $file_paths "\n"]
 
-
-label .drag_me -text "Drag This:\n $file_path" -bg cyan  -font {Monospace 16 bold}
+label .drag_me -text "Drag This:\n $file_paths" -bg cyan  -font {Monospace 16 bold}
 pack .drag_me -padx 10 -pady 10
 
 tkdnd::drag_source register .drag_me text/uri-list
 
-bind .drag_me <<DragInitCmd>> [list trigger_drag $file_path]
+bind .drag_me <<DragInitCmd>> [list trigger_drag $file_paths]
 
 bind .drag_me <Button-2> { exit }
 bind .drag_me <Button-3> { exit }
 bind . <Escape> { exit }
 
-proc trigger_drag {file_path} {
-    set uri "file://$file_path"
-    return [list copy text/uri-list $uri]
+proc trigger_drag {file_paths} {
+    return [list copy text/uri-list $file_paths]
 }
