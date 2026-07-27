38ad5b79015b641eb3424be0df0a53b8591a46f6
jonghyouk yun <ageldama@gmail.com>
2026-07-27 23:05:13 +0900

    --wip-- [skip ci]

--- dotfiles/local_bin/caps-to-ctrl.sh
+++ dotfiles/local_bin/caps-to-ctrl.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then
+if [ -n "$1" ] && [ "$1" -gt 0 ]; then
     sleep "$1"
 fi
 
-xmodmap -e 'keycode 66 = Control_L'
-xmodmap -e 'clear Lock'
-xmodmap -e 'add Control = Control_L'
+map_capslock_as_control() {
+    xmodmap -e 'keycode 66 = Control_L'
+    xmodmap -e 'clear Lock'
+    xmodmap -e 'add Control = Control_L'
+}
 
+map_capslock_as_control
 
+while [ -n "$1" ] && [ "$1" -eq -1 ]; do
+    if xmodmap -pm|grep -q -E '^control.+0x42'; then
+        exit 0
+    fi
+    map_capslock_as_control
+done
